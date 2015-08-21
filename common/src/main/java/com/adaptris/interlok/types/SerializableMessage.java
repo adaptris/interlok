8bba9945046e2c5202e3946132dab1dbd338a381
Lewin Chan <lewin.chan@adaptris.com>
2015-08-21 16:28:28 +0100

    INTERLOK-591 Added a removeHeader() method

--- common/src/main/java/com/adaptris/interlok/types/SerializableMessage.java
+++ common/src/main/java/com/adaptris/interlok/types/SerializableMessage.java
@@ -3,6 +3,11 @@ package com.adaptris.interlok.types;
 import java.io.Serializable;
 import java.util.Map;
 
+/**
+ * Basic message implementation that can be serialized.
+ * 
+ * 
+ */
 public interface SerializableMessage extends Serializable {
 
   public String getUniqueId();
@@ -13,11 +18,32 @@ public interface SerializableMessage extends Serializable {
 
   public void setPayload(String payload);
 
+  /**
+   * Returns a view of all the existing headers associated with the message.
+   * <p>
+   * Any changes to the returned {@link Map} are not guaranteed to be reflected in underlying map.
+   * You should treat the returned Map as a read only view of the current message headers. Use
+   * {@link #addMessageHeader(String, String)} or {@link #removeMessageHeader(String)} to manipulate
+   * individual headers.
+   * </p>
+   * 
+   * @return a read only view of the messages.
+   */
   public Map<String, String> getMessageHeaders();
 
+  /**
+   * Overwrite all the headers.
+   * <p>
+   * Clear and overwrite all the headers
+   * </p>
+   * 
+   * @param metadata
+   */
   public void setMessageHeaders(Map<String, String> metadata);
 
   public void addMessageHeader(String key, String value);
+
+  public void removeMessageHeader(String key);
 
   public String getPayloadEncoding();
 
