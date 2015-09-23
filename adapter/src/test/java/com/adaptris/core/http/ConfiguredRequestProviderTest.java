2f2ccfd6776ff370bc606ae7a31bf8d8081689de
Lewin Chan <lewin.chan@adaptris.com>
2015-09-23 14:00:17 +0100

    INTERLOK-663 Added tests for ConfiguredRequestProvider.

---
+++ adapter/src/test/java/com/adaptris/core/http/ConfiguredRequestProviderTest.java
@@ -0,0 +1,32 @@
+package com.adaptris.core.http;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.adaptris.core.AdaptrisMessage;
+import com.adaptris.core.DefaultMessageFactory;
+import com.adaptris.core.http.RequestMethodProvider.RequestMethod;
+
+public class ConfiguredRequestProviderTest {
+
+  @Before
+  public void setUp() throws Exception {}
+
+  @After
+  public void tearDown() throws Exception {
+
+  }
+
+  @Test
+  public void testGetMethod() {
+    ConfiguredRequestMethodProvider prov = new ConfiguredRequestMethodProvider(RequestMethod.GET);
+    AdaptrisMessage msg = new DefaultMessageFactory().newMessage("");
+    assertEquals(RequestMethod.GET, prov.getMethod(msg));
+  }
+
+
+
+}
