4b5c6c77cee5910a3997d7222e7c831c2f9263d2
Lewin Chan <lewin.chan@adaptris.com>
2015-09-25 11:56:24 +0100

    INTERLOK-668 Fixed up javadocs. INTERLOK-663 #time 5m

--- adapter/src/main/java/com/adaptris/core/http/client/net/CompositeRequestHeaders.java
+++ adapter/src/main/java/com/adaptris/core/http/client/net/CompositeRequestHeaders.java
@@ -14,7 +14,7 @@ import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamImplicit;
 
 /**
- * Implementation of {@link RequestHeaderHandler} that uses nested providers to add headers to a {@link
+ * Implementation of {@link RequestHeaderProvider} that uses nested providers to add headers to a {@link
  * HttpURLConnection}.
  * 
  * <p>This implementation is primarily so that you can mix and match both static and metadata driven headers; the order in which
