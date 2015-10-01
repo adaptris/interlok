1fcaadc1da1a027a0fe6dcff26531051ece7a900
aaron-mcgrath-adp <aaron.mcgrath@adaptris.com>
2015-10-01 12:26:27 +0100

    INTERLOK-674 #time 10m Added some basic javadoc.

--- common/src/main/java/com/adaptris/interlok/config/DataDestination.java
+++ common/src/main/java/com/adaptris/interlok/config/DataDestination.java
@@ -3,6 +3,21 @@ package com.adaptris.interlok.config;
 import com.adaptris.interlok.InterlokException;
 import com.adaptris.interlok.types.InterlokMessage;
 
+/**
+ * <p>
+ * DataDestinations are a generic configuration item, that allow you to specify where data from needed by a service
+ * will come from or be saved to.
+ * </p>
+ * <p>
+ * Please check the service documentation to make sure that service supports DataDestinations.
+ * </p>
+ * <p>
+ * A typical use might be for something like the {@link XPathService}, which allows you ti use DataDestinations
+ * to specify the location of the source xml, xpath expression and the target destination for any results returned.
+ * </p>
+ * @author Aaron
+ *
+ */
 public interface DataDestination {
   
   public Object getData(InterlokMessage message) throws InterlokException;
