26ce488a7b1e87cd94b501e8e2e507c670603c85
Lewin Chan <lewin.chan@adaptris.com>
2016-01-12 13:30:22 +0000

    INTERLOK-752 Added a MetadataDocumentCopier INTERLOK-752 #time 3h

---
+++ adapter/src/main/java/com/adaptris/core/services/splitter/MetadataDocumentCopier.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright 2015 Adaptris Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package com.adaptris.core.services.splitter;
+
+import static org.apache.commons.lang.StringUtils.isEmpty;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import com.adaptris.core.AdaptrisMessage;
+import com.adaptris.core.AdaptrisMessageFactory;
+import com.adaptris.core.CoreException;
+import com.adaptris.core.util.Args;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+/**
+ * Implementation of {@link MessageSplitter} which creates multiple instances of the same document based on a metadata key.
+ * 
+ * <p>
+ * The split messages will always contain the entire document, the metadata key containing the number of documents to generate.
+ * </p>
+ * 
+ * @config metadata-document-copier
+ */
+@XStreamAlias("metadata-document-copier")
+public class MetadataDocumentCopier extends MessageSplitterImp {
+
+  @NotNull
+  @NotBlank
+  private String metadataKey = null;
+  private String indexMetadataKey;
+
+  public MetadataDocumentCopier() {
+  }
+
+
+  public MetadataDocumentCopier(String key) {
+    this(key, null);
+  }
+
+  public MetadataDocumentCopier(String key, String indexKey) {
+    this();
+    setMetadataKey(key);
+    setIndexMetadataKey(indexKey);
+  }
+
+  @Override
+  public CloseableIterable<AdaptrisMessage> splitMessage(AdaptrisMessage msg) throws CoreException {
+    int size = toInteger(msg.getMetadataValue(getMetadataKey()));     
+    return new Copier(msg, size);
+  }
+
+  private static int toInteger(String s) {
+    if (isEmpty(s)) {
+      return 0;
+    }
+    return Double.valueOf(s).intValue();
+  }
+
+  /**
+   * Set the metadata key to use to for generating msgs.
+   *
+   * @param xp the metadata key
+   */
+  public void setMetadataKey(String key) {
+    metadataKey = Args.notEmpty(key, "metadata key");
+  }
+
+  public String getMetadataKey() {
+    return metadataKey;
+  }
+
+
+  public String getIndexMetadataKey() {
+    return indexMetadataKey;
+  }
+
+  /**
+   * The metadata key storing the current index of the message from {@link #setMetadataKey(String)}
+   * will be stored (zero based index).
+   * 
+   * @param s the index metadata key to set
+   */
+  public void setIndexMetadataKey(String s) {
+    indexMetadataKey = s;
+  }
+
+  /**
+   * Not ThreadSafe, Not Reentrant; but means we can have arbitrarily large amounts of metadata-splits.
+   */
+  private class Copier implements CloseableIterable<AdaptrisMessage>, Iterator<AdaptrisMessage> {
+    private final AdaptrisMessage msg;
+    private final AdaptrisMessageFactory factory;
+    private final int maxCount;
+
+    private AdaptrisMessage nextMessage;
+    private int currentCount = 0;
+
+    public Copier(AdaptrisMessage msg, int maxItems) {
+      this.msg = msg;
+      maxCount = maxItems;
+      factory = selectFactory(msg);
+    }
+
+    @Override
+    public Iterator<AdaptrisMessage> iterator() {
+      // This Iterable can only be Iterated once so multiple iterators are unsupported. That's why
+      // it's safe to just return this in this method without constructing a new Iterator.
+      return this;
+    }
+
+    @Override
+    public boolean hasNext() {
+      if (nextMessage == null) {
+        nextMessage = createNext();
+      }
+      return nextMessage != null;
+    }
+
+    @Override
+    public AdaptrisMessage next() {
+      AdaptrisMessage ret = nextMessage;
+      nextMessage = null;
+      return ret;
+    }
+
+    private AdaptrisMessage createNext() {
+      if (currentCount >= maxCount)
+        return null;
+
+      AdaptrisMessage splitMsg = factory.newMessage(msg.getPayload());
+      splitMsg.setContentEncoding(msg.getContentEncoding());
+      copyMetadata(msg, splitMsg);
+      if (!isEmpty(getIndexMetadataKey())) {
+        splitMsg.addMetadata(getIndexMetadataKey(), String.valueOf(currentCount));
+      }
+      currentCount++;
+      return splitMsg;
+    }
+
+    @Override
+    public void close() throws IOException {
+    }
+
+    @Override
+    public void remove() {
+      throw new UnsupportedOperationException();
+    }
+
+  };
+}
---
+++ adapter/src/test/java/com/adaptris/core/services/splitter/MetadataDocumentCopierTest.java
@@ -0,0 +1,188 @@
+/*
+ * Copyright 2015 Adaptris Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package com.adaptris.core.services.splitter;
+
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.adaptris.core.AdaptrisMessage;
+import com.adaptris.core.AdaptrisMessageFactory;
+import com.adaptris.core.stubs.MockMessageProducer;
+
+@SuppressWarnings("deprecation")
+public class MetadataDocumentCopierTest extends SplitterCase {
+
+  private static final String METADATA_KEY_INDEX = "MyMetadataKeyIndex";
+
+  private static final String METADATA_KEY = "MyMetadataKey";
+
+  private static Log logR = LogFactory.getLog(MetadataDocumentCopierTest.class);
+
+  public MetadataDocumentCopierTest(java.lang.String testName) {
+    super(testName);
+  }
+
+  @Override
+  protected void setUp() throws Exception {
+
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+  }
+
+  @Override
+  protected String createBaseFileName(Object object) {
+    return super.createBaseFileName(object) + "-MetadataDocumentCopier";
+  }
+
+  @Override
+  protected Object retrieveObjectForSampleConfig() {
+    return null; // over-rides retrieveServices below instead
+  }
+
+  @Override
+  protected List retrieveObjectsForSampleConfig() {
+    return createExamples(new MetadataDocumentCopier(METADATA_KEY));
+  }
+
+  @Override
+  protected String getExampleCommentHeader(Object o) {
+    return super.getExampleCommentHeader(o) + "\n<!-- \n Creates 'n' copies of a message where 'n' is determined by\n"
+        + "the value associated with 'MyMetadataKey'\n" + "-->\n";
+  }
+
+  @Override
+  protected MetadataDocumentCopier createSplitterForTests() {
+    return new MetadataDocumentCopier();
+  }
+
+  public void testConstructors() throws Exception {
+    MetadataDocumentCopier splitter = new MetadataDocumentCopier(METADATA_KEY);
+    assertEquals(METADATA_KEY, splitter.getMetadataKey());
+    splitter = new MetadataDocumentCopier();
+    assertNull(splitter.getMetadataKey());
+  }
+
+  public void testSetMetadataKey() throws Exception {
+    MetadataDocumentCopier splitter = new MetadataDocumentCopier();
+    assertNull(splitter.getMetadataKey());
+    splitter.setMetadataKey("FRED");
+    assertEquals("FRED", splitter.getMetadataKey());
+    try {
+      splitter.setMetadataKey(null);
+    } catch (IllegalArgumentException e) {
+
+    }
+    try {
+      splitter.setMetadataKey("");
+    } catch (IllegalArgumentException e) {
+
+    }
+  }
+
+  public void testSetIndexMetadataKey() throws Exception {
+    MetadataDocumentCopier splitter = new MetadataDocumentCopier();
+    assertNull(splitter.getIndexMetadataKey());
+    splitter.setIndexMetadataKey("FRED");
+    assertEquals("FRED", splitter.getIndexMetadataKey());
+    splitter.setIndexMetadataKey(null);
+    assertNull(splitter.getIndexMetadataKey());
+    splitter.setIndexMetadataKey("");
+    assertEquals("", splitter.getIndexMetadataKey());
+  }
+
+  public void testSplit() throws Exception {
+    final int expectedSplitCount = 10;
+    AdaptrisMessage msg = AdaptrisMessageFactory.getDefaultInstance().newMessage(LINE);
+    String obj = "ABCDEFG";
+    msg.addObjectHeader(obj, obj);
+    msg.addMetadata(METADATA_KEY, String.valueOf(expectedSplitCount));
+    MetadataDocumentCopier splitter = new MetadataDocumentCopier(METADATA_KEY, METADATA_KEY_INDEX);
+    CloseableIterable<AdaptrisMessage> result = splitter.splitMessage(msg);
+    int count = 0;
+    for (AdaptrisMessage m : result) {
+      assertFalse("No Object Metadata", m.getObjectMetadata().containsKey(obj));
+      assertEquals(String.valueOf(count), m.getMetadataValue(METADATA_KEY_INDEX));
+      count++;
+    }
+    assertEquals(expectedSplitCount, count);
+  }
+
+  public void testSplit_NoIndex() throws Exception {
+    final int expectedSplitCount = 10;
+    AdaptrisMessage msg = AdaptrisMessageFactory.getDefaultInstance().newMessage(LINE);
+    String obj = "ABCDEFG";
+    msg.addObjectHeader(obj, obj);
+    msg.addMetadata(METADATA_KEY, String.valueOf(expectedSplitCount));
+    MetadataDocumentCopier splitter = new MetadataDocumentCopier(METADATA_KEY);
+    CloseableIterable<AdaptrisMessage> result = splitter.splitMessage(msg);
+    int count = 0;
+    for (AdaptrisMessage m : result) {
+      assertFalse("No Object Metadata", m.getObjectMetadata().containsKey(obj));
+      assertFalse(m.containsKey(METADATA_KEY_INDEX));
+      count++;
+    }
+    assertEquals(expectedSplitCount, count);
+  }
+
+  public void testSplit_EmptyMetadata() throws Exception {
+    AdaptrisMessage msg = AdaptrisMessageFactory.getDefaultInstance().newMessage(LINE);
+    String obj = "ABCDEFG";
+    msg.addObjectHeader(obj, obj);
+    MetadataDocumentCopier splitter = new MetadataDocumentCopier(METADATA_KEY, METADATA_KEY_INDEX);
+    CloseableIterable<AdaptrisMessage> result = splitter.splitMessage(msg);
+    int count = 0;
+    for (AdaptrisMessage m : result) {
+      assertFalse("No Object Metadata", m.getObjectMetadata().containsKey(obj));
+      count++;
+    }
+    assertEquals(0, count);
+  }
+
+  public void testSplitWithObjectMetadata() throws Exception {
+    final int expectedSplitCount = 10;
+    AdaptrisMessage msg = AdaptrisMessageFactory.getDefaultInstance().newMessage(LINE);
+    String obj = "ABCDEFG";
+    msg.addObjectHeader(obj, obj);
+    msg.addMetadata(METADATA_KEY, String.valueOf(expectedSplitCount));
+    MetadataDocumentCopier splitter = new MetadataDocumentCopier(METADATA_KEY, METADATA_KEY_INDEX);
+    splitter.setCopyObjectMetadata(true);
+    CloseableIterable<AdaptrisMessage> result = splitter.splitMessage(msg);
+    int count = 0;
+    for (AdaptrisMessage m : result) {
+      assertTrue("Object Metadata", m.getObjectMetadata().containsKey(obj));
+      assertEquals(obj, m.getObjectMetadata().get(obj));
+      assertEquals(String.valueOf(count), m.getMetadataValue(METADATA_KEY_INDEX));
+      count++;
+    }
+    assertEquals(expectedSplitCount, count);
+  }
+
+  public void testService() throws Exception {
+    final int expectedSplitCount = 3;
+    AdaptrisMessage msg = AdaptrisMessageFactory.getDefaultInstance().newMessage(LINE);
+    msg.addMetadata(METADATA_KEY, String.valueOf(expectedSplitCount));
+    BasicMessageSplitterService service = createBasic(new MetadataDocumentCopier(METADATA_KEY, METADATA_KEY_INDEX));
+    MockMessageProducer producer = new MockMessageProducer();
+    service.setProducer(producer);
+    execute(service, msg);
+    assertEquals(expectedSplitCount, producer.getMessages().size());
+  }
+}
