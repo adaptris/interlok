b8c59ee1021db632095858bb127e2e8f5a36df7c
aaron-mcgrath-adp <aaron.mcgrath@adaptris.com>
2016-03-07 12:29:35 +0000

    INTERLOK-832 #time 1d Seems it can take up to a second to fully close a connection.  I have added a delay that should make the test pass.

--- adapter/src/test/java/com/adaptris/core/jdbc/AdvancedJdbcPooledConnectionTest.java
+++ adapter/src/test/java/com/adaptris/core/jdbc/AdvancedJdbcPooledConnectionTest.java
@@ -35,61 +35,66 @@ public class AdvancedJdbcPooledConnectionTest extends DatabaseConnectionCase<Adv
   }
 
 //INTERLOK-107
-// public void testConnectionDataSource_Poolsize() throws Exception {
-//   String originalThread = Thread.currentThread().getName();
-//   Thread.currentThread().setName("testConnectionDataSource_Poolsize");    
-//       
-//   AdvancedJdbcPooledConnection con = configure(createConnection());
-//   con.setConnectUrl("jdbc:derby:memory:" + GUID.safeUUID() + ";create=true");
-//   con.setDriverImp("org.apache.derby.jdbc.EmbeddedDriver");
-//   con.getConnectionPoolProperties().add(new KeyValuePair(PooledConnectionProperties.acquireIncrement.name(), "5"));
-//   con.getConnectionPoolProperties().add(new KeyValuePair(PooledConnectionProperties.minPoolSize.name(), "10"));
-//   con.getConnectionPoolProperties().add(new KeyValuePair(PooledConnectionProperties.maxPoolSize.name(), "50"));
-//   con.getConnectionPoolProperties().add(new KeyValuePair(PooledConnectionProperties.checkoutTimeout.name(), "30000"));
-//   
-//   try {
-//     LifecycleHelper.init(con);
-//     LifecycleHelper.start(con);
-//     ComboPooledDataSource poolDs = (ComboPooledDataSource) con.asDataSource();
-//     assertEquals(0, poolDs.getNumBusyConnections());
-//     Connection c1 = poolDs.getConnection();
-//     log.info("1 get: NumConnections=" + poolDs.getNumConnections() + ", NumBusyConnnections=" + poolDs.getNumBusyConnections() + ", NumIdleConnections" + poolDs.getNumIdleConnections());
-//     c1.close();
-//     log.info("1 close: NumConnections=" + poolDs.getNumConnections() + ", NumBusyConnnections=" + poolDs.getNumBusyConnections() + ", NumIdleConnections" + poolDs.getNumIdleConnections());
-//     c1 = poolDs.getConnection();
-//     log.info("1 get (again): NumConnections=" + poolDs.getNumConnections() + ", NumBusyConnnections=" + poolDs.getNumBusyConnections() + ", NumIdleConnections" + poolDs.getNumIdleConnections());
-//     Connection c2 = poolDs.getConnection();
-//     log.info("2 get: NumConnections=" + poolDs.getNumConnections() + ", NumBusyConnnections=" + poolDs.getNumBusyConnections() + ", NumIdleConnections" + poolDs.getNumIdleConnections());
-//     Connection c3 = poolDs.getConnection();
-//     log.info("3 get: NumConnections=" + poolDs.getNumConnections() + ", NumBusyConnnections=" + poolDs.getNumBusyConnections() + ", NumIdleConnections" + poolDs.getNumIdleConnections());
-//     Connection c4 = poolDs.getConnection();
-//     log.info("4 get: NumConnections=" + poolDs.getNumConnections() + ", NumBusyConnnections=" + poolDs.getNumBusyConnections() + ", NumIdleConnections" + poolDs.getNumIdleConnections());
-//     Connection c5 = poolDs.getConnection();
-//     log.info("5 get: NumConnections=" + poolDs.getNumConnections() + ", NumBusyConnnections=" + poolDs.getNumBusyConnections() + ", NumIdleConnections" + poolDs.getNumIdleConnections());
-//     Connection c6 = poolDs.getConnection();
-//     log.info("6 get: NumConnections=" + poolDs.getNumConnections() + ", NumBusyConnnections=" + poolDs.getNumBusyConnections() + ", NumIdleConnections" + poolDs.getNumIdleConnections());
-//
-//     Connection c7 = poolDs.getConnection();
-//     log.info("7 get: NumConnections=" + poolDs.getNumConnections() + ", NumBusyConnnections=" + poolDs.getNumBusyConnections() + ", NumIdleConnections" + poolDs.getNumIdleConnections());
-//
-//     assertEquals(7, poolDs.getNumBusyConnections());
-//     c1.close();
-//     c2.close();
-//     c3.close();
-//     c4.close();
-//     c5.close();
-//     c6.close();
-//     c7.close();
-//     Thread.sleep(2000);
-//     log.info("closed: NumConnections=" + poolDs.getNumConnections() + ", NumBusyConnnections=" + poolDs.getNumBusyConnections() + ", NumIdleConnections" + poolDs.getNumIdleConnections());
-//
-//     assertEquals(0, poolDs.getNumBusyConnections());
-//   }
-//   finally {
-//     Thread.currentThread().setName(originalThread);   
-//     LifecycleHelper.stop(con);
-//     LifecycleHelper.close(con);
-//   }
-//
-// }
+ public void testConnectionDataSource_Poolsize() throws Exception {
+   String originalThread = Thread.currentThread().getName();
+   Thread.currentThread().setName("testConnectionDataSource_Poolsize");    
+       
+   AdvancedJdbcPooledConnection con = configure(createConnection());
+   con.setConnectUrl("jdbc:derby:memory:" + GUID.safeUUID() + ";create=true");
+   con.setDriverImp("org.apache.derby.jdbc.EmbeddedDriver");
+   con.getConnectionPoolProperties().add(new KeyValuePair(PooledConnectionProperties.acquireIncrement.name(), "5"));
+   con.getConnectionPoolProperties().add(new KeyValuePair(PooledConnectionProperties.minPoolSize.name(), "10"));
+   con.getConnectionPoolProperties().add(new KeyValuePair(PooledConnectionProperties.maxPoolSize.name(), "50"));
+   con.getConnectionPoolProperties().add(new KeyValuePair(PooledConnectionProperties.checkoutTimeout.name(), "30000"));
+   
+   try {
+     LifecycleHelper.init(con);
+     LifecycleHelper.start(con);
+     ComboPooledDataSource poolDs = (ComboPooledDataSource) con.asDataSource();
+     assertEquals(0, poolDs.getNumBusyConnections());
+     Connection c1 = null;
+     try {
+       c1 = poolDs.getConnection();
+       log.info("1 get: NumConnections=" + poolDs.getNumConnections() + ", NumBusyConnnections=" + poolDs.getNumBusyConnections() + ", NumIdleConnections" + poolDs.getNumIdleConnections());
+     } finally {
+       Thread.sleep(1500);
+       c1.close();
+     }
+     log.info("1 close: NumConnections=" + poolDs.getNumConnections() + ", NumBusyConnnections=" + poolDs.getNumBusyConnections() + ", NumIdleConnections" + poolDs.getNumIdleConnections());
+     c1 = poolDs.getConnection();
+     log.info("1 get (again): NumConnections=" + poolDs.getNumConnections() + ", NumBusyConnnections=" + poolDs.getNumBusyConnections() + ", NumIdleConnections" + poolDs.getNumIdleConnections());
+     Connection c2 = poolDs.getConnection();
+     log.info("2 get: NumConnections=" + poolDs.getNumConnections() + ", NumBusyConnnections=" + poolDs.getNumBusyConnections() + ", NumIdleConnections" + poolDs.getNumIdleConnections());
+     Connection c3 = poolDs.getConnection();
+     log.info("3 get: NumConnections=" + poolDs.getNumConnections() + ", NumBusyConnnections=" + poolDs.getNumBusyConnections() + ", NumIdleConnections" + poolDs.getNumIdleConnections());
+     Connection c4 = poolDs.getConnection();
+     log.info("4 get: NumConnections=" + poolDs.getNumConnections() + ", NumBusyConnnections=" + poolDs.getNumBusyConnections() + ", NumIdleConnections" + poolDs.getNumIdleConnections());
+     Connection c5 = poolDs.getConnection();
+     log.info("5 get: NumConnections=" + poolDs.getNumConnections() + ", NumBusyConnnections=" + poolDs.getNumBusyConnections() + ", NumIdleConnections" + poolDs.getNumIdleConnections());
+     Connection c6 = poolDs.getConnection();
+     log.info("6 get: NumConnections=" + poolDs.getNumConnections() + ", NumBusyConnnections=" + poolDs.getNumBusyConnections() + ", NumIdleConnections" + poolDs.getNumIdleConnections());
+
+     Connection c7 = poolDs.getConnection();
+     log.info("7 get: NumConnections=" + poolDs.getNumConnections() + ", NumBusyConnnections=" + poolDs.getNumBusyConnections() + ", NumIdleConnections" + poolDs.getNumIdleConnections());
+
+     assertEquals(7, poolDs.getNumBusyConnections());
+     c1.close();
+     c2.close();
+     c3.close();
+     c4.close();
+     c5.close();
+     c6.close();
+     c7.close();
+     Thread.sleep(2000);
+     log.info("closed: NumConnections=" + poolDs.getNumConnections() + ", NumBusyConnnections=" + poolDs.getNumBusyConnections() + ", NumIdleConnections" + poolDs.getNumIdleConnections());
+
+     assertEquals(0, poolDs.getNumBusyConnections());
+   }
+   finally {
+     Thread.currentThread().setName(originalThread);   
+     LifecycleHelper.stop(con);
+     LifecycleHelper.close(con);
+   }
+
+ }
 }
