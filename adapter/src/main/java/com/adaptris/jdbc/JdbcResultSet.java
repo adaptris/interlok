4bca35ec896e00fd979df9225b943344ca608de2
Gerco Dries <gerco@gdries.nl>
2016-03-15 15:34:28 -0400

    INTERLOK-847 Convert JdbcResultSet wrapper to use live ResultSet objects instead of spooling them into memory in advance.

--- adapter/src/main/java/com/adaptris/jdbc/JdbcResultSet.java
+++ adapter/src/main/java/com/adaptris/jdbc/JdbcResultSet.java
@@ -16,37 +16,133 @@
 
 package com.adaptris.jdbc;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class JdbcResultSet {
 
-  private List<JdbcResultRow> rows;
+  private ResultSet resultSet;
   
-  public JdbcResultSet() {
-    this.setRows(new ArrayList<JdbcResultRow>());
-  }
-
-  public List<JdbcResultRow> getRows() {
-    return rows;
-  }
-
-  public void setRows(List<JdbcResultRow> rows) {
-    this.rows = rows;
-  }
-
-  public void addRow(JdbcResultRow row) {
-    this.getRows().add(row);
+  public JdbcResultSet(ResultSet resultSet) {
+    this.resultSet = resultSet;
   }
   
-  public String toString() {
-    StringBuffer buffer = new StringBuffer();
-    buffer.append(this.getClass().getSimpleName() + " " + this.getRows().size() + " rows\n");
+  public Iterable<JdbcResultRow> getRows() {
+    return new Iterable<JdbcResultRow>() {
+
+      @Override
+      public Iterator<JdbcResultRow> iterator() {
+        try {
+          switch(resultSet.getType()) {
+          case ResultSet.TYPE_FORWARD_ONLY:
+            return new ForwardOnlyResultSetIterator(resultSet);
+            
+          case ResultSet.TYPE_SCROLL_SENSITIVE:
+          case ResultSet.TYPE_SCROLL_INSENSITIVE:
+            return new ScrollableResultSetIterator(resultSet);
+            
+          default:
+            throw new RuntimeException("ResultSet was of unknown type");
+          }
+        } catch(SQLException e) {
+          throw new RuntimeException(e);
+        }
+      }
+    };
+  }
+  
+  /**
+   * ScrollableResultSetIterator works only for ResultSets that support .isAfterLast() but
+   * follows the ResultSet API most closely by not 'pre-taking' any elements until next()
+   * is called.
+   */
+  private static class ScrollableResultSetIterator implements Iterator<JdbcResultRow> {
+    private final ResultSet resultSet;
     
-    for(JdbcResultRow row : this.getRows()) {
-      buffer.append("\t\t" + row.toString() + "\n");
+    private ScrollableResultSetIterator(ResultSet resultSet) {
+      this.resultSet = resultSet;
     }
     
-    return buffer.toString();
+    @Override
+    public boolean hasNext() {
+      try {
+        return !resultSet.isAfterLast();
+      } catch (SQLException e) {
+        return false;
+      }
+    }
+
+    @Override
+    public JdbcResultRow next() {
+      try {
+        if(resultSet.next()) {
+          return mapRow(resultSet);
+        } else {
+          throw new NoSuchElementException();
+        }
+      } catch (SQLException e) {
+        throw new RuntimeException(e);
+      }
+    }
   }
+  
+  /**
+   * ForwardOnlyResultSetIterator works for ResultSets that don't support .isAfterLast(), which
+   * is optional for TYPE_FORWARD_ONLY ResultSets. It works by 'pre-taking' an element and hasNext()
+   * reports whether that was successful. A side effect of that is that constructing this ResultSet
+   * can throw an Exception. 
+   */
+  private static class ForwardOnlyResultSetIterator implements Iterator<JdbcResultRow> {
+    private final ResultSet resultSet;
+    private JdbcResultRow nextRow;
+    
+    private ForwardOnlyResultSetIterator(ResultSet resultSet) {
+      this.resultSet = resultSet;
+      nextRow = takeNext();
+    }
+    
+    @Override
+    public boolean hasNext() {
+      return nextRow != null;
+    }
+
+    @Override
+    public JdbcResultRow next() {
+      if(hasNext()) {
+        JdbcResultRow tmp = nextRow;
+        nextRow = takeNext();
+        return tmp;
+      } else {
+        throw new NoSuchElementException();
+      }
+    }
+    
+    private JdbcResultRow takeNext() {
+      try {
+        if(resultSet.next()) {
+          return mapRow(resultSet);
+        } else {
+          return null;
+        }
+      } catch (SQLException e) {
+        throw new RuntimeException(e);
+      }
+    }
+  }
+  
+  private static JdbcResultRow mapRow(ResultSet resultSet) throws SQLException {
+    ResultSetMetaData rsmd = resultSet.getMetaData();
+    int columnCount = rsmd.getColumnCount();
+
+    JdbcResultRow row = new JdbcResultRow();
+    for(int counter = 1; counter <= columnCount; counter ++) {
+      row.setFieldValue(rsmd.getColumnName(counter), resultSet.getObject(counter));
+    }
+
+    return row;
+  }
+
 }
