01c2b8fdbfcb2b7d5b906961c06e0777324d8bb2
mcwarman <matthew.warman@adaptris.com>
2016-11-03 09:18:40 +0000

    [service-tester] Fix imports RemoveNodePreprocessor

--- service-tester/src/main/java/com/adaptris/tester/runtime/services/preprocessor/RemoveNodePreprocessor.java
+++ service-tester/src/main/java/com/adaptris/tester/runtime/services/preprocessor/RemoveNodePreprocessor.java
@@ -2,12 +2,10 @@ package com.adaptris.tester.runtime.services.preprocessor;
 
 import com.adaptris.core.util.DocumentBuilderFactoryBuilder;
 import com.adaptris.core.util.XmlHelper;
-import com.adaptris.util.text.xml.SimpleNamespaceContext;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
-import javax.xml.namespace.NamespaceContext;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 
