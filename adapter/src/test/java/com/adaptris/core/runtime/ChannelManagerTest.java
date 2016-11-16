4e6b26ec93854737d6094a0380d39f42e8a5cc71
Ashley Anderson <ashley.anderson@reedbusiness.com>
2016-11-16 16:06:42 +0000

    Use non-deprecated methods seems to fix test

--- adapter/src/test/java/com/adaptris/core/runtime/ChannelManagerTest.java
+++ adapter/src/test/java/com/adaptris/core/runtime/ChannelManagerTest.java
@@ -1336,10 +1336,8 @@ public class ChannelManagerTest extends ComponentManagerCase {
 
       mBeanServer.addNotificationListener(channelObj, listener, null, null);
       ChannelManagerMBean channelManagerProxy = JMX.newMBeanProxy(mBeanServer, channelObj, ChannelManagerMBean.class);
-      channelManagerProxy.requestStart();
-      Thread.sleep(1000);
-      channelManagerProxy.requestStop();
-      Thread.sleep(1000);
+      channelManagerProxy.requestStart(2000);
+      channelManagerProxy.requestStop(2000);
       assertEquals(StoppedState.getInstance(), channelManagerProxy.getComponentState());
       listener.waitForMessages(2);
       assertEquals(2, listener.getNotifications().size());
