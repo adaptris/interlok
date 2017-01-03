aadccd2d54b375018f0f5800e129bf04979e4e30
Ashley Anderson <ashley.anderson@reedbusiness.com>
2017-01-03 11:26:11 +0000

    Initial attempt at setting classpath
    Try to build the URLs for the classpath from the current system
    classpath (which should just be the directory of this JAR).

--- common/src/main/java/com/adaptris/interlok/management/ClassLoaderBootstrap.java
+++ common/src/main/java/com/adaptris/interlok/management/ClassLoaderBootstrap.java
@@ -1,5 +1,6 @@
 package com.adaptris.interlok.management;
 
+import java.io.File;
 import java.net.URL;
 import java.net.URLClassLoader;
 
@@ -17,47 +18,61 @@ public class ClassLoaderBootstrap {
 	/**
 	 * Main boot method.
 	 * 
-	 * @throws Exception If anything bad happens.
+	 * @throws Exception
+	 *           If anything bad happens.
 	 */
 	@SuppressWarnings("resource")
 	public void boot() throws Exception {
 		/*
 		 * put opendmk_jdmkrt_jar.jar and opendmk_jmxremote_optional_jar.jar on command line
 		 */
-		/*
-		 * TODO Remove hardcoded path; either by using a config file that
-		 * points to the path/lib directory or each jar file as necessary
-		 */
-		final URL[] urls = new URL[]
+
+		final ClassLoader sysClassLoader = ClassLoader.getSystemClassLoader();
+		URL[] urls = ((URLClassLoader)sysClassLoader).getURLs();
+		String configDir = null;
+		String libDir = null;
+
+		for (final URL url : urls) {
+			final File file = new File(url.getFile());
+			if (file.isDirectory()) {
+				configDir = "file:///" + new File(file.getPath() + "/../config/").getCanonicalPath();
+				libDir = "file:///" + new File(file.getPath() + "/../lib/").getCanonicalPath();
+				break;
+			}
+		}
+
+		urls = new URL[]
 				{
-					new URL("file:///C:/Adaptris/Interlok-3.4.0/config/"),
-					new URL("file:///C:/Adaptris/Interlok-3.4.0/lib/jetty-all.jar"),
-					new URL("file:///C:/Adaptris/Interlok-3.4.0/lib/"),
+					new URL(configDir),
+					new URL(libDir + "jetty-all.jar"),
+					new URL(libDir),
 					
-					new URL("file:///C:/Adaptris/Interlok-3.4.0/lib/geronimo-jms_1.1_spec.jar"),
-					new URL("file:///C:/Adaptris/Interlok-3.4.0/lib/geronimo-servlet_3.0_spec.jar"),
+					new URL(libDir + "geronimo-jms_1.1_spec.jar"),
+					new URL(libDir + "geronimo-servlet_3.0_spec.jar"),
 					
-					new URL("file:///C:/Adaptris/Interlok-3.4.0/lib/log4j-1.2-api.jar"),
-					new URL("file:///C:/Adaptris/Interlok-3.4.0/lib/log4j-api.jar"),
-					new URL("file:///C:/Adaptris/Interlok-3.4.0/lib/log4j-core.jar"),
-					new URL("file:///C:/Adaptris/Interlok-3.4.0/lib/log4j-slf4j-impl.jar"),
+					new URL(libDir + "log4j-1.2-api.jar"),
+					new URL(libDir + "log4j-api.jar"),
+					new URL(libDir + "log4j-core.jar"),
+					new URL(libDir + "log4j-slf4j-impl.jar"),
 					
-					new URL("file:///C:/Adaptris/Interlok-3.4.0/lib/adp-common.jar")
+					new URL(libDir + "adp-common.jar")
 				};
 		final URLClassLoader parentClassLoader = new URLClassLoader(urls, null);
 
-		final URLClassLoader runtimeClassLoader = new URLClassLoader(new URL[] { new URL("file:///C:/Adaptris/Interlok-3.4.0/lib/adp-core.jar") }, parentClassLoader);
-		
+		final URLClassLoader runtimeClassLoader = new URLClassLoader(new URL[] { new URL(libDir + "adp-core.jar") }, parentClassLoader);
+
 		final Class<?> simpleBootstrap = runtimeClassLoader.loadClass("com.adaptris.core.management.SimpleBootstrap");
 		simpleBootstrap.newInstance();
 	}
-	
+
 	/**
 	 * Entry point.
 	 * 
-	 * @param argv Command line arguments; ignored.
+	 * @param argv
+	 *          Command line arguments; ignored.
 	 * 
-	 * @throws Exception If anything bad happens.
+	 * @throws Exception
+	 *           If anything bad happens.
 	 */
 	public static void main(final String[] argv) throws Exception {
 		new ClassLoaderBootstrap().boot();
