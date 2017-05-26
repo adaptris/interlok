9e2324c77af8a1eefeedd75996689ed1b5239d15
Lewin Chan <lewin.chan@adaptris.com>
2017-05-26 14:11:43 +0100

    Added the ability to base64 decode/encode metadata so we can decode Basic authorization headers.

---
+++ adapter/src/test/java/com/adaptris/core/services/metadata/Base64MetadataDecodeTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2015 Adaptris Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package com.adaptris.core.services.metadata;
+
+import com.adaptris.core.AdaptrisMessage;
+import com.adaptris.core.AdaptrisMessageFactory;
+import com.adaptris.util.text.Base64ByteTranslator;
+
+public class Base64MetadataDecodeTest extends MetadataServiceExample {
+
+  private static final String METADATA_KEY = "key";
+  
+  public Base64MetadataDecodeTest(String name) {
+    super(name);
+  }
+
+  public void testService() throws Exception {
+    Base64DecodeMetadataService service = new Base64DecodeMetadataService(METADATA_KEY);
+    AdaptrisMessage msg = AdaptrisMessageFactory.getDefaultInstance().newMessage("asdfghjk");
+
+    String metadataValue = new Base64ByteTranslator().translate("Hello World".getBytes());
+    msg.addMetadata(METADATA_KEY, metadataValue);
+
+    execute(service, msg);
+    assertNotSame(metadataValue, msg.getMetadataValue(METADATA_KEY));
+    assertEquals("Hello World", msg.getMetadataValue(METADATA_KEY));
+  }
+
+  public void testService_Encoding() throws Exception {
+    Base64DecodeMetadataService service = new Base64DecodeMetadataService(METADATA_KEY);
+    AdaptrisMessage msg = AdaptrisMessageFactory.getDefaultInstance().newMessage("asdfghjk", "UTF-8");
+    String metadataValue = new Base64ByteTranslator().translate("Hello World".getBytes("UTF-8"));
+    msg.addMetadata(METADATA_KEY, metadataValue);
+
+    execute(service, msg);
+
+    assertNotSame(metadataValue, msg.getMetadataValue(METADATA_KEY));
+    assertEquals("Hello World", msg.getMetadataValue(METADATA_KEY));
+  }
+
+  @Override
+  protected Object retrieveObjectForSampleConfig() {
+    return new Base64DecodeMetadataService(".*MetadataKeyRegularExpression.*");
+  }
+}
---
+++ adapter/src/test/java/com/adaptris/core/services/metadata/Base64MetadataEncodeTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2015 Adaptris Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package com.adaptris.core.services.metadata;
+
+import com.adaptris.core.AdaptrisMessage;
+import com.adaptris.core.AdaptrisMessageFactory;
+
+public class Base64MetadataEncodeTest extends MetadataServiceExample {
+
+  private static final String METADATA_KEY = "key";
+  private static final String METADATA_VALUE = "Hello World";
+  
+  public Base64MetadataEncodeTest(String name) {
+    super(name);
+  }
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+  }
+
+
+  private AdaptrisMessage createMessage(String encoding) throws Exception {
+    AdaptrisMessage msg = AdaptrisMessageFactory.getDefaultInstance().newMessage("asdfghjk", encoding);
+    msg.addMetadata(METADATA_KEY, METADATA_VALUE);
+    msg.addMetadata("yetAnotherKey", "");
+    return msg;
+  }
+
+
+  public void testService() throws Exception {
+    Base64EncodeMetadataService service = new Base64EncodeMetadataService(METADATA_KEY);
+    AdaptrisMessage msg = createMessage(null);
+    execute(service, msg);
+    assertNotSame(METADATA_VALUE, msg.getMetadataValue(METADATA_KEY));
+  }
+
+  public void testService_Encoding() throws Exception {
+    Base64EncodeMetadataService service = new Base64EncodeMetadataService(METADATA_KEY);
+    AdaptrisMessage msg = createMessage("UTF-8");
+    execute(service, msg);
+    assertNotSame(METADATA_VALUE, msg.getMetadataValue(METADATA_KEY));
+  }
+
+  @Override
+  protected Object retrieveObjectForSampleConfig() {
+    return new Base64EncodeMetadataService(".*MetadataKeyRegularExpression.*");
+  }
+}
