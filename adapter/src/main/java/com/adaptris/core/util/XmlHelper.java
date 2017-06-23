24028ca4d9342992bcfd22e8cc73be3827a91f92
Lewin Chan <lewin.chan@adaptris.com>
2017-06-23 16:23:24 +0100

    INTERLOK-1692 Dodgy expectation about the non-nullness of DBFB

--- adapter/src/main/java/com/adaptris/core/util/XmlHelper.java
+++ adapter/src/main/java/com/adaptris/core/util/XmlHelper.java
@@ -199,7 +199,7 @@ public class XmlHelper {
   private static DocumentBuilder newDocumentBuilder(DocumentBuilderFactoryBuilder cfg) throws ParserConfigurationException {
     DocumentBuilderFactoryBuilder b = defaultIfNull(cfg);
     DocumentBuilderFactory domFactory = b.configure(DocumentBuilderFactory.newInstance());
-    DocumentBuilder builder = cfg.configure(domFactory.newDocumentBuilder());
+    DocumentBuilder builder = b.configure(domFactory.newDocumentBuilder());
     builder.setErrorHandler(new DefaultErrorHandler());
     return builder;
   }
