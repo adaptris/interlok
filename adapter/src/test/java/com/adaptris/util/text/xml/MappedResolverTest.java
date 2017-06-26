8680c06134d4e33341c2c3e775635ba3a85217ef
Lewin Chan <lewin.chan@adaptris.com>
2017-06-26 11:17:23 +0100

    INTERLOK-1692 Removed relative test
    - Because the unit tests are actually all run from ../ the relative
    path is wrong; even using relative=yes in the build.xml for the property
    doesn't help matters because the real user.dir is still ../

--- adapter/src/test/java/com/adaptris/util/text/xml/MappedResolverTest.java
+++ adapter/src/test/java/com/adaptris/util/text/xml/MappedResolverTest.java
@@ -26,7 +26,6 @@ public class MappedResolverTest extends BaseCase {
   private static final String PUBLIC_ID = "-//Mort Bay Consulting//DTD Configure//EN";
   private static final String SYSTEM_ID = "http://www.eclipse.org/jetty/configure.dtd";
   private static final String KEY_MAPPED_URL = "resolver.mapped.url";
-  private static final String KEY_MAPPED_RELATIVE = "resolver.mapped.relative";
 
   public MappedResolverTest(String name) {
     super(name);
@@ -36,16 +35,6 @@ public class MappedResolverTest extends BaseCase {
   public void testEntityResolver_Mapping_Absolute() throws Exception {
     MappedResolver resolver = new MappedResolver();
     resolver.getMappings().add(new KeyValuePair(SYSTEM_ID, PROPERTIES.getProperty(KEY_MAPPED_URL)));
-    assertNotNull(resolver.resolveEntity(PUBLIC_ID, SYSTEM_ID));
-    assertEquals(1, resolver.size());
-    assertNotNull(resolver.resolveEntity(PUBLIC_ID, SYSTEM_ID));
-    assertEquals(1, resolver.size());
-  }
-
-  @Test
-  public void testEntityResolver_Mapping_Relative() throws Exception {
-    MappedResolver resolver = new MappedResolver();
-    resolver.getMappings().add(new KeyValuePair(SYSTEM_ID, PROPERTIES.getProperty(KEY_MAPPED_RELATIVE)));
     assertNotNull(resolver.resolveEntity(PUBLIC_ID, SYSTEM_ID));
     assertEquals(1, resolver.size());
     assertNotNull(resolver.resolveEntity(PUBLIC_ID, SYSTEM_ID));
