868aed9f92635613d6e4992173cf522865d61efc
Lewin Chan <lewin.chan@adaptris.com>
2017-06-28 09:49:40 +0100

    INTERLOK-1583 Added a Branching version of HttpRequestService INTERLOK-1583 #time 3h
    Added a branching version of HttpRequestService which allows you to
    select the next service id based on the http status response.

---
+++ adapter/src/main/java/com/adaptris/core/http/client/net/RangeMatch.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2017 Adaptris Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+package com.adaptris.core.http.client.net;
+
+import com.adaptris.annotation.DisplayOrder;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+/**
+ * Implementation of {@link StatusEvaluator} for use with {@link BranchingHttpRequestService}.
+ * <p>
+ * Matches a service id against range of values e.g. lower=200, upper=299 to match all "success" codes.
+ * </p>
+ * 
+ * @config http-status-range-match
+ *
+ */
+@XStreamAlias("http-status-range-match")
+@DisplayOrder(order = {"serviceId", "lower", "upper"})
+public class RangeMatch extends StatusEvaluatorImpl {
+
+  private int lower;
+  private int upper;
+
+  public RangeMatch() {
+    super();
+  }
+
+  public RangeMatch(int lower, int upper, String serviceId) {
+    this();
+    setLower(lower);
+    setUpper(upper);
+    setServiceId(serviceId);
+  }
+
+  @Override
+  public boolean matches(int responseCode) {
+    return lower <= responseCode && responseCode <= upper;
+  }
+
+  /**
+   * @return the lowerBoundary
+   */
+  public int getLower() {
+    return lower;
+  }
+
+  /**
+   * @param lowerBoundary the lowerBoundary to set
+   */
+  public void setLower(int lowerBoundary) {
+    this.lower = lowerBoundary;
+  }
+
+  /**
+   * @return the upperBoundary
+   */
+  public int getUpper() {
+    return upper;
+  }
+
+  /**
+   * @param upperBoundary the upperBoundary to set
+   */
+  public void setUpper(int upperBoundary) {
+    this.upper = upperBoundary;
+  }
+
+
+}
---
+++ adapter/src/test/java/com/adaptris/core/http/client/net/StatusMatchTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2017 Adaptris Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+package com.adaptris.core.http.client.net;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class StatusMatchTest {
+
+  @Test
+  public void testExactMatch() throws Exception {
+    ExactMatch match = new ExactMatch(200, "200 OK");
+    assertEquals(200, match.getStatusCode());
+    assertEquals("200 OK", match.getServiceId());
+    assertEquals("200 OK", match.serviceId());
+    assertTrue(match.matches(200));
+    assertFalse(match.matches(201));
+  }
+
+  @Test
+  public void testRangeMatch() throws Exception {
+    RangeMatch match = new RangeMatch(200, 299, "200 OK");
+    assertEquals(200, match.getLower());
+    assertEquals(299, match.getUpper());
+    assertEquals("200 OK", match.getServiceId());
+    assertEquals("200 OK", match.serviceId());
+    assertTrue(match.matches(200));
+    assertTrue(match.matches(201));
+    assertFalse(match.matches(400));
+    assertFalse(match.matches(300));
+  }
+}
