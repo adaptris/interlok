5986edd6ad9aa62d3297ef6bc47174edbbf8643c
Lewin Chan <lewin.chan@adaptris.com>
2017-11-23 14:29:08 +0000

    Added explicit tests to check behaviour for INTERLOK-1978

--- adapter/src/test/java/com/adaptris/util/text/xml/XPathTest.java
+++ adapter/src/test/java/com/adaptris/util/text/xml/XPathTest.java
@@ -19,6 +19,10 @@ import static com.adaptris.util.text.xml.SimpleNamespaceContextTest.createNamesp
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNull;
+
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
 
 import org.junit.After;
 import org.junit.Before;
@@ -32,7 +36,34 @@ import com.adaptris.core.util.XmlHelper;
 import net.sf.saxon.xpath.XPathFactoryImpl;
 
 public class XPathTest {
-  private static String XML = "<root><test att='1'>one</test><test att='2'>two</test></root>";
+  private final static String XML = "<root><test att='1'>one</test><test att='2'>two</test><node>one<child/>two</node></root>";
+  private static final String XML_WITH_EMPTY_NODES = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n" + 
+      "<root>\n" + 
+      "  <segment_PIX>\n" + 
+      "    <segment_Contents>\n" + 
+      "      <record_>\n" + 
+      "        <PXREF1/>\n" + 
+      "      </record_>\n" + 
+      "      <record_>\n" + 
+      "        <PXREF1/>\n" + 
+      "      </record_>\n" + 
+      "      <record_>\n" + 
+      "        <PXREF1/>\n" + 
+      "      </record_>\n" + 
+      "      <record_>\n" + 
+      "        <PXREF1>91/01</PXREF1>\n" + 
+      "      </record_>\n" + 
+      "      <record_>\n" + 
+      "        <PXREF1>91/01</PXREF1>\n" + 
+      "      </record_>\n" + 
+      "      <record_>\n" + 
+      "        <PXREF1>91/01</PXREF1>\n" + 
+      "      </record_>\n" + 
+      "    </segment_Contents>\n" + 
+      "  </segment_PIX>\n" + 
+      "</root>";
+  
+  private static final String XPATH_EMPTY_NODES = "/root/segment_PIX/segment_Contents/record_/PXREF1";
 
   @Before
   public void setUp() throws Exception {
@@ -64,13 +95,56 @@ public class XPathTest {
     assertEquals("test", xpath.selectSingleTextItem(d, "//*[@att='2'][1]/local-name()"));
   }
 
+  @Test(expected = javax.xml.xpath.XPathExpressionException.class)
+  public void testXpath_Xpath2Unsupported() throws Exception {
+    // so we expect XPathFactory.newInstance() equivalent to fail as it's not XPATH2.0
+    Document d = XmlHelper.createDocument(XML, DocumentBuilderFactoryBuilder.newInstance());
+    XPathFactory.newInstance().newXPath().evaluate("//*[@att='2'][1]/local-name()", d, XPathConstants.STRING);
+  }
+
   @Test
   public void testSelectMultipleTextItems() throws Exception {
-    XPath xpath = new XPath();
+    XPath xpath = new XPath(SimpleNamespaceContext.create(SimpleNamespaceContextTest.createNamespaceEntries()));
     Document d = XmlHelper.createDocument(XML, DocumentBuilderFactoryBuilder.newInstance());
     String[] vals = xpath.selectMultipleTextItems(d, "/root/test");
+    assertEquals(2, vals.length);
     assertEquals("one", vals[0]);
     assertEquals("two", vals[1]);
+  }
+
+  @Test
+  public void testSelectMultipleTextItems_Attributes() throws Exception {
+    XPath xpath = new XPath();
+    Document d = XmlHelper.createDocument(XML, DocumentBuilderFactoryBuilder.newInstance());
+    String[] vals = xpath.selectMultipleTextItems(d, "/root/test/@att");
+    assertEquals(2, vals.length);
+    assertEquals("1", vals[0]);
+    assertEquals("2", vals[1]);
+  }
+
+  @Test
+  public void testSelectMultipleTextItems_TextNode() throws Exception {
+    XPath xpath = new XPath();
+    Document d = XmlHelper.createDocument(XML, DocumentBuilderFactoryBuilder.newInstance());
+    String[] vals = xpath.selectMultipleTextItems(d, "/root/node/text()");
+    assertEquals(2, vals.length);
+    assertEquals("one", vals[0]);
+    assertEquals("two", vals[1]);
+  }
+
+  @Test
+  public void testSelectMultipleTextItems_WithNullValues() throws Exception {
+    XPath xpath = new XPath();
+    Document d = XmlHelper.createDocument(XML_WITH_EMPTY_NODES, DocumentBuilderFactoryBuilder.newInstance());
+    String[] vals = xpath.selectMultipleTextItems(d, XPATH_EMPTY_NODES);
+    // first 3 nodes will be "null"
+    assertEquals(6, vals.length);
+    for (int i = 0; i < 3; i++) {
+      assertNull(vals[i]);
+    }
+    for (int i = 3; i < 6; i++) {
+      assertEquals("91/01", vals[i]);
+    }
   }
 
   @Test
