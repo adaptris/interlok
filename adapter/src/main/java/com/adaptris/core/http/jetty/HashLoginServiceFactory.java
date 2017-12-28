46121282e47c3235aea8d5f5db68d339f2682b85
Lewin Chan <lewin.chan@adaptris.com>
2017-12-28 11:10:12 +0000

    Updated to refer to the eclipse jetty documentation page.

--- adapter/src/main/java/com/adaptris/core/http/jetty/HashLoginServiceFactory.java
+++ adapter/src/main/java/com/adaptris/core/http/jetty/HashLoginServiceFactory.java
@@ -14,7 +14,33 @@ import com.adaptris.core.util.Args;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 
 /**
- * Implementation that provides a {@link HashLoginService}.
+ * Allows you to configure a {@code HashLoginService} for use with Jetty.
+ * <p>
+ * This simply exposes the Jetty HashLoginService as Interlok configuration. The documentation from
+ * <a href="https://wiki.eclipse.org/Jetty/Tutorial/Realms">the eclipse jetty site</a> should always be considered canonical. The
+ * HashLoginService is a realm whose authentication and authorization information is stored in a properties file. Each line in the
+ * file contains a username, a password, and zero or more role assignments. The format is <pre>
+ * {@code 
+username: password[,rolename ...]
+username: password[,rolename ...]
+ * }
+ * </pre> where:
+ * <ul>
+ * <li><strong>username</strong> is the user's unique identity</li>
+ * <li><strong>password</strong> is the user's (possibly obfuscated or MD5 encrypted) password;</li>
+ * <li><strong>rolename</strong> is the user's role.</li>
+ * </ul>
+ * <p>
+ * If you need to secure the passwords in your properties file then you can follow
+ * <a href= "https://www.eclipse.org/jetty/documentation/9.3.x/configuring-security-secure-passwords.html">the jetty
+ * documentation</a> to obfuscate/hash the password. <pre>
+ * {@code
+$ java -cp ./lib/jetty-util.jar  org.eclipse.jetty.util.security.Password myusername MySuperSecretPassword
+OBF:1o4o1zly1rhf1zst1y0s1vu91uvk1ldu1w1c1sot1y7z1sox1w261ldo1uum1vv11y0y1zsx1riz1zlk1o5y
+MD5:d418c3e96fb98e73fd603a8b6134edda
+CRYPT:my8hdCDBVkNU.
+ * }
+ * </pre>
  * 
  * @author lchan
  *
