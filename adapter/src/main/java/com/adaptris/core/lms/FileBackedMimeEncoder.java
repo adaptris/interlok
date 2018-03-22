7ff2e83888c7ea299ed4fa887253c43e2cd0230c
Lewin Chan <lewin.chan@adaptris.com>
2018-03-22 19:31:04 +0000

    INTERLOK-2128 Added FileBackedMimeEncoder which uses MultipartFileInput

---
+++ adapter/src/main/java/com/adaptris/core/lms/FileBackedMimeEncoder.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2015 Adaptris Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package com.adaptris.core.lms;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.mail.internet.MimeBodyPart;
+
+import org.apache.commons.io.IOUtils;
+
+import com.adaptris.annotation.DisplayOrder;
+import com.adaptris.core.AdaptrisMessage;
+import com.adaptris.core.AdaptrisMessageFactory;
+import com.adaptris.core.CoreConstants;
+import com.adaptris.core.CoreException;
+import com.adaptris.core.MimeEncoderImpl;
+import com.adaptris.core.util.Args;
+import com.adaptris.core.util.ExceptionHelper;
+import com.adaptris.util.text.mime.MultiPartFileInput;
+import com.adaptris.util.text.mime.MultiPartOutput;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+/**
+ * Implementation of {@code AdaptrisMessageEncoder} that stores payload and metadata as a mime-encoded multipart message.
+ * <p>
+ * The expected use case for this is within a {@link LargeFsConsumer} or {@link LargeFsProducer} so that you can capture the
+ * metadata along with any exceptions in addition to the payload. Since the data may be arbitrarily large, this implementation works
+ * <strong>only with files</strong>.
+ * </p>
+ * 
+ * @config file-backed-mime-encoder
+ */
+@XStreamAlias("file-backed-mime-encoder")
+@DisplayOrder(order = {"payloadEncoding", "metadataEncoding", "retainUniqueId"})
+public class FileBackedMimeEncoder extends MimeEncoderImpl {
+
+  public FileBackedMimeEncoder() {
+    super();
+  }
+
+  @Override
+  public void writeMessage(AdaptrisMessage msg, Object target) throws CoreException {
+    try {
+      File baseFile = asFile(target);
+      // Use the message unique id as the message id.
+      MultiPartOutput output = new MultiPartOutput(msg.getUniqueId());
+      AdaptrisMessageFactory factory = currentMessageFactory();
+      output.addPart(payloadAsMimePart(msg), PAYLOAD_CONTENT_ID);
+      output.addPart(getMetadata(msg), getMetadataEncoding(), METADATA_CONTENT_ID);
+      if (msg.getObjectHeaders().containsKey(CoreConstants.OBJ_METADATA_EXCEPTION)) {
+        output.addPart(asMimePart((Exception) msg.getObjectHeaders().get(CoreConstants.OBJ_METADATA_EXCEPTION)),
+            EXCEPTION_CONTENT_ID);
+      }
+      try (OutputStream out = new FileOutputStream(baseFile)) {
+        // If we are file backed, then lets assume we're large, and we stream to disk first...
+        if (factory instanceof FileBackedMessageFactory) {
+          output.writeTo(out, ((FileBackedMessageFactory) factory).createTempFile(msg));
+        } else {
+          output.writeTo(out);
+        }
+      }
+    } catch (Exception e) {
+      throw ExceptionHelper.wrapCoreException(e);
+    }
+  }
+
+  @Override
+  public AdaptrisMessage readMessage(Object source) throws CoreException {
+    AdaptrisMessage msg = null;
+
+    try {
+      msg = currentMessageFactory().newMessage();
+      File baseFile = asFile(source);
+      MultiPartFileInput input = new MultiPartFileInput(baseFile);
+      MimeBodyPart payloadPart = Args.notNull(input.getBodyPart(PAYLOAD_CONTENT_ID), "payload");
+      MimeBodyPart metadataPart = Args.notNull(input.getBodyPart(METADATA_CONTENT_ID), "metadata");
+      try (InputStream payloadIn = payloadPart.getInputStream();
+          InputStream metadata = metadataPart.getInputStream();
+          OutputStream out = msg.getOutputStream()) {
+        IOUtils.copy(payloadIn, out);
+        msg.setMetadata(getMetadataSet(metadata));
+      }
+      if (retainUniqueId()) {
+        msg.setUniqueId(input.getMessageID());
+      }
+    } catch (Exception e) {
+      throw ExceptionHelper.wrapCoreException(e);
+    }
+    return msg;
+  }
+
+  private File asFile(Object o) {
+    if (!(o instanceof File)) {
+      throw new IllegalArgumentException("FileBackedMimeEncoder can only encode/decode from a File");
+    }
+    return (File) o;
+  }
+
+}
---
+++ adapter/src/test/java/com/adaptris/core/lms/FileBackedMimeEncoderTest.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2015 Adaptris Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package com.adaptris.core.lms;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Properties;
+
+import org.junit.Test;
+
+import com.adaptris.core.AdaptrisMessage;
+import com.adaptris.core.AdaptrisMessageFactory;
+import com.adaptris.core.CoreConstants;
+import com.adaptris.core.CoreException;
+import com.adaptris.core.stubs.TempFileUtils;
+import com.adaptris.util.GuidGenerator;
+import com.adaptris.util.text.mime.MultiPartOutput;
+
+public class FileBackedMimeEncoderTest {
+  private static final String STANDARD_PAYLOAD = "Pack My Box with a dozen Liquor jugs";
+  private static final String STANDARD_PAYLOAD_NON_JUST_ALPHA = "The quick brown fox jumps over the lazy dog\t\r\n";
+
+  private static final String METADATA_VALUE = "value";
+  private static final String METADATA_KEY = "key";
+
+  @Test(expected = CoreException.class)
+  public void testEncode_NotFile() throws Exception {
+    FileBackedMimeEncoder mimeEncoder = new FileBackedMimeEncoder();
+
+    mimeEncoder.writeMessage(AdaptrisMessageFactory.getDefaultInstance().newMessage(STANDARD_PAYLOAD), new Object());
+  }
+
+  @Test(expected = CoreException.class)
+  public void testDecode_NotFile() throws Exception {
+    FileBackedMimeEncoder mimeEncoder = new FileBackedMimeEncoder();
+    mimeEncoder.readMessage(new Object());
+  }
+
+  @Test
+  public void testRoundTrip() throws Exception {
+    FileBackedMimeEncoder mimeEncoder = new FileBackedMimeEncoder();
+    mimeEncoder.setRetainUniqueId(true);
+
+    AdaptrisMessage msg = AdaptrisMessageFactory.getDefaultInstance().newMessage(STANDARD_PAYLOAD);
+    msg.addMetadata(METADATA_KEY, METADATA_VALUE);
+    File outputFile = TempFileUtils.createTrackedFile(msg);
+    mimeEncoder.writeMessage(msg, outputFile);
+
+    AdaptrisMessage result = mimeEncoder.readMessage(outputFile);
+    assertEquals(msg.getUniqueId(), result.getUniqueId());
+    assertEquals(METADATA_VALUE, result.getMetadataValue(METADATA_KEY));
+    assertEquals(STANDARD_PAYLOAD, result.getContent());
+  }
+
+  @Test
+  public void testRoundTrip_FileBackedFactory() throws Exception {
+    FileBackedMimeEncoder mimeEncoder = new FileBackedMimeEncoder();
+    mimeEncoder.registerMessageFactory(new FileBackedMessageFactory());
+
+    AdaptrisMessage msg = AdaptrisMessageFactory.getDefaultInstance().newMessage(STANDARD_PAYLOAD);
+    msg.addMetadata(METADATA_KEY, METADATA_VALUE);
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    File outputFile = TempFileUtils.createTrackedFile(msg);
+    mimeEncoder.writeMessage(msg, outputFile);
+
+    AdaptrisMessage result = mimeEncoder.readMessage(outputFile);
+    assertEquals(METADATA_VALUE, result.getMetadataValue(METADATA_KEY));
+    assertEquals(STANDARD_PAYLOAD, result.getContent());
+  }
+
+  @Test
+  public void testDecode_IgnoreExtraParts() throws Exception {
+    FileBackedMimeEncoder mimeEncoder = new FileBackedMimeEncoder();
+    mimeEncoder.registerMessageFactory(new FileBackedMessageFactory());
+
+    AdaptrisMessage result = mimeEncoder.readMessage(generateOutput());
+    assertEquals(METADATA_VALUE, result.getMetadataValue(METADATA_KEY));
+    assertEquals(STANDARD_PAYLOAD, result.getContent());
+  }
+
+  @Test
+  public void testEncode_WithException() throws Exception {
+    FileBackedMimeEncoder mimeEncoder = new FileBackedMimeEncoder();
+
+    AdaptrisMessage msg = AdaptrisMessageFactory.getDefaultInstance().newMessage(STANDARD_PAYLOAD);
+    msg.addMetadata(METADATA_KEY, METADATA_VALUE);
+    msg.getObjectHeaders().put(CoreConstants.OBJ_METADATA_EXCEPTION, new Exception("testEncode_WithException"));
+    File outputFile = TempFileUtils.createTrackedFile(msg);
+    mimeEncoder.writeMessage(msg, outputFile);
+
+    AdaptrisMessage result = mimeEncoder.readMessage(outputFile);
+    assertNotSame(msg.getUniqueId(), result.getUniqueId());
+    assertEquals(METADATA_VALUE, result.getMetadataValue(METADATA_KEY));
+    assertEquals(STANDARD_PAYLOAD, result.getContent());
+  }
+
+  private File generateOutput() throws Exception {
+    File file = TempFileUtils.createTrackedFile(this);
+    MultiPartOutput output = new MultiPartOutput(new GuidGenerator().getUUID());
+    output.addPart(STANDARD_PAYLOAD.getBytes(), "base64", "AdaptrisMessage/payload");
+    Properties p = new Properties();
+    p.setProperty(METADATA_KEY, METADATA_VALUE);
+    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+    p.store(bytes, "");
+    bytes.close();
+    output.addPart(bytes.toByteArray(), "base64", "AdaptrisMessage/metadata");
+    output.addPart(STANDARD_PAYLOAD_NON_JUST_ALPHA.getBytes(), "base64", "Dude/SomeOtherPart");
+    try (FileOutputStream out = new FileOutputStream(file)) {
+      output.writeTo(out);
+    }
+    return file;
+  }
+}
