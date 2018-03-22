cfa3fae52f5c117f65d52469c00ecfce08f6cdcf
Lewin Chan <lewin.chan@adaptris.com>
2018-03-22 19:24:40 +0000

    INTERLOK-2128 Added hierarchy.

--- adapter/src/main/java/com/adaptris/core/MimeEncoder.java
+++ adapter/src/main/java/com/adaptris/core/MimeEncoder.java
@@ -16,26 +16,19 @@
 
 package com.adaptris.core;
 
-import static com.adaptris.core.util.MetadataHelper.convertFromProperties;
-import static com.adaptris.core.util.MetadataHelper.convertToProperties;
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.PrintStream;
-import java.util.Properties;
-import java.util.Set;
 
-import javax.validation.constraints.Pattern;
+import javax.mail.internet.MimeBodyPart;
 
-import org.apache.commons.lang.builder.ToStringBuilder;
-import org.apache.commons.lang.builder.ToStringStyle;
+import org.apache.commons.io.IOUtils;
 
-import com.adaptris.annotation.AdvancedConfig;
 import com.adaptris.annotation.DisplayOrder;
-import com.adaptris.annotation.InputFieldDefault;
+import com.adaptris.core.util.Args;
+import com.adaptris.core.util.ExceptionHelper;
 import com.adaptris.util.text.mime.MultiPartInput;
 import com.adaptris.util.text.mime.MultiPartOutput;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
@@ -58,21 +51,7 @@ import com.thoughtworks.xstream.annotations.XStreamAlias;
  */
 @XStreamAlias("mime-encoder")
 @DisplayOrder(order = {"payloadEncoding", "metadataEncoding", "retainUniqueId"})
-public class MimeEncoder extends AdaptrisMessageEncoderImp {
-
-  private static final String PAYLOAD_CONTENT_ID = "AdaptrisMessage/payload";
-  private static final String METADATA_CONTENT_ID = "AdaptrisMessage/metadata";
-  private static final String EXCEPTION_CONTENT_ID = "AdaptrisMessage/exception";
-
-  @Pattern(regexp = "base64|quoted-printable|uuencode|x-uuencode|x-uue|binary|7bit|8bit")
-  @AdvancedConfig
-  private String metadataEncoding;
-  @Pattern(regexp = "base64|quoted-printable|uuencode|x-uuencode|x-uue|binary|7bit|8bit")
-  @AdvancedConfig
-  private String payloadEncoding;
-  @AdvancedConfig
-  @InputFieldDefault(value = "false")
-  private Boolean retainUniqueId;
+public class MimeEncoder extends MimeEncoderImpl {
 
   public MimeEncoder() {
     super();
@@ -84,83 +63,54 @@ public class MimeEncoder extends AdaptrisMessageEncoderImp {
     setMetadataEncoding(metadataEncoding);
     setPayloadEncoding(payloadEncoding);
   }
-  /**
-   * Encode the <code>AdaptrisMessage</code> object.
-   * <p>
-   * The target object is assumed to be of the type <code>OutputStream</code>
-   * </p>
-   *
-   * @see AdaptrisMessageEncoder#writeMessage(AdaptrisMessage, Object)
-   */
-  public void writeMessage(AdaptrisMessage msg, Object target)
-      throws CoreException {
 
+  @Override
+  public void writeMessage(AdaptrisMessage msg, Object target) throws CoreException {
     try {
       if (!(target instanceof OutputStream)) {
-        throw new IllegalArgumentException(
-            "MimeEncoder can only encode to an OutputStream");
+        throw new IllegalArgumentException("MimeEncoder can only encode to an OutputStream");
       }
       OutputStream encodedOutput = (OutputStream) target;
       // Use the message unique id as the message id.
       MultiPartOutput output = new MultiPartOutput(msg.getUniqueId());
-      output.addPart(msg.getPayload(), payloadEncoding, PAYLOAD_CONTENT_ID);
-      output.addPart(MimeEncoder.getMetadata(msg), metadataEncoding,
-          METADATA_CONTENT_ID);
-      if (msg.getObjectHeaders().containsKey(
-          CoreConstants.OBJ_METADATA_EXCEPTION)) {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        PrintStream pout = new PrintStream(out);
-        Exception e = (Exception) msg.getObjectHeaders().get(
-            CoreConstants.OBJ_METADATA_EXCEPTION);
-        e.printStackTrace(pout);
-        pout.flush();
-        output.addPart(out.toByteArray(), EXCEPTION_CONTENT_ID);
+      AdaptrisMessageFactory factory = currentMessageFactory();
+      output.addPart(payloadAsMimePart(msg), PAYLOAD_CONTENT_ID);
+      output.addPart(getMetadata(msg), getMetadataEncoding(), METADATA_CONTENT_ID);
+      if (msg.getObjectHeaders().containsKey(CoreConstants.OBJ_METADATA_EXCEPTION)) {
+        output.addPart(asMimePart((Exception) msg.getObjectHeaders().get(CoreConstants.OBJ_METADATA_EXCEPTION)),
+            EXCEPTION_CONTENT_ID);
       }
-      encodedOutput.write(output.getBytes());
+      output.writeTo(encodedOutput);
       encodedOutput.flush();
-    }
-    catch (Exception e) {
-      throw new CoreException("Could not encode the AdaptrisMessage object", e);
+    } catch (Exception e) {
+      throw ExceptionHelper.wrapCoreException(e);
     }
   }
 
-  /**
-   * Decode into an <code>AdaptrisMessage</code> object.
-   * <p>
-   * The source object is assumed to be of the type <code>InputStream</code>
-   * </p>
-   *
-   * @see com.adaptris.core.AdaptrisMessageEncoder#readMessage(java.lang.Object)
-   */
+  @Override
   public AdaptrisMessage readMessage(Object source) throws CoreException {
     AdaptrisMessage msg = null;
 
     try {
       msg = currentMessageFactory().newMessage();
       if (!(source instanceof InputStream)) {
-        throw new IllegalArgumentException(
-            "MimeEncoder can only decode from an OutputStream");
+        throw new IllegalArgumentException("MimeEncoder can only decode from an OutputStream");
       }
       InputStream encodedInput = (InputStream) source;
-      MultiPartInput input = new MultiPartInput(encodedInput);
-      byte[] readBytes = input.getPart(PAYLOAD_CONTENT_ID);
-      if (readBytes == null) {
-        throw new IOException("No Payload found");
+      MultiPartInput input = new MultiPartInput(encodedInput, false);
+      MimeBodyPart payloadPart = Args.notNull(input.getBodyPart(PAYLOAD_CONTENT_ID), "payload");
+      MimeBodyPart metadataPart = Args.notNull(input.getBodyPart(METADATA_CONTENT_ID), "metadata");
+      try (InputStream payloadIn = payloadPart.getInputStream();
+          InputStream metadata = metadataPart.getInputStream();
+          OutputStream out = msg.getOutputStream()) {
+        IOUtils.copy(payloadIn, out);
+        msg.setMetadata(getMetadataSet(metadata));
       }
-      msg.setPayload(readBytes);
-      readBytes = input.getPart(METADATA_CONTENT_ID);
-      if (readBytes == null) {
-        throw new IOException("No Metadata Found");
-      }
-      msg.setMetadata(getMetadataSet(new ByteArrayInputStream(readBytes)));
-
       if (retainUniqueId()) {
         msg.setUniqueId(input.getName());
       }
-    }
-    catch (Exception e) {
-      throw new CoreException(
-          "Could not parse supplied bytes into an AdaptrisMessage object", e);
+    } catch (Exception e) {
+      throw ExceptionHelper.wrapCoreException(e);
     }
     return msg;
   }
@@ -200,100 +150,4 @@ public class MimeEncoder extends AdaptrisMessageEncoderImp {
     return msg;
   }
 
-  /**
-   * <p>
-   * Returns the payload MIME encoding.
-   * </p>
-   *
-   * @return the payload MIME encoding
-   */
-  public String getPayloadEncoding() {
-    return payloadEncoding;
-  }
-
-  /**
-   * <p>
-   * Returns the metadata MIME encoding.
-   * </p>
-   *
-   * @return the metadata MIME encoding
-   */
-  public String getMetadataEncoding() {
-    return metadataEncoding;
-  }
-
-  /**
-   * <p>
-   * Sets the payload MIME encoding.
-   * </p>
-   *
-   * @param encoding the payload MIME encoding
-   */
-  public void setPayloadEncoding(String encoding) {
-    payloadEncoding = encoding;
-  }
-
-  /**
-   * <p>
-   * Sets the metadata MIME encoding.
-   * </p>
-   *
-   * @param encoding the metadata MIME encoding
-   */
-  public void setMetadataEncoding(String encoding) {
-    metadataEncoding = encoding;
-  }
-
-  /**
-   * <p>
-   * Returns true if the original ID of a decoded message should be retained for the new message.
-   * </p>
-   * 
-   * @return true if the original ID of a decoded message should be retained for the new message
-   */
-  public Boolean getRetainUniqueId() {
-    return retainUniqueId;
-  }
-
-  /**
-   * <p>
-   * Sets whether the original ID of a decoded message should be retained for the new message.
-   * </p>
-   * 
-   * @param b true if the original ID should be retained
-   */
-  public void setRetainUniqueId(Boolean b) {
-    retainUniqueId = b;
-  }
-
-  public boolean retainUniqueId() {
-    return getRetainUniqueId() != null ? getRetainUniqueId().booleanValue() : false;
-  }
-
-  /**
-   * Get metadata out of an AdaptrisMessage
-   *
-   * @return a byte array representing the adaptris message
-   */
-  private static byte[] getMetadata(AdaptrisMessage msg) throws IOException {
-
-    ByteArrayOutputStream out = new ByteArrayOutputStream();
-    Properties metadata = convertToProperties(msg.getMetadata());
-    metadata.store(out, "");
-    out.close();
-    return out.toByteArray();
-  }
-
-  private static Set<MetadataElement> getMetadataSet(InputStream in) throws IOException {
-    Properties p = new Properties();
-    p.load(in);
-    return convertFromProperties(p);
-  }
-
-  /** @see java.lang.Object#toString() */
-  @Override
-  public String toString() {
-    return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).append("PayloadEncoding", getPayloadEncoding())
-        .append("MetadataEncoding", getMetadataEncoding()).append("retainUniqueId", retainUniqueId()).toString();
-  }
 }
