95e111652b86b4bbcbc67bd0e10b74da44a70679
Lewin Chan <lewin.chan@adaptris.com>
2019-01-25 11:24:37 +0000

    Add tests for default methods.

---
+++ interlok-core/src/test/java/com/adaptris/core/cache/CacheDefaultMethodsTest.java
@@ -0,0 +1,65 @@
+package com.adaptris.core.cache;
+
+import java.io.Serializable;
+import org.junit.Test;
+import com.adaptris.core.CoreException;
+import com.adaptris.core.util.LifecycleHelper;
+
+public class CacheDefaultMethodsTest implements Cache {
+
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void testDefaultPutStringObject() throws Exception {
+    try {
+      LifecycleHelper.initAndStart(this);
+      put("hello", new Object());
+    } finally {
+      LifecycleHelper.stopAndClose(this);
+    }
+  }
+
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void testDefaultGetKeys() throws Exception {
+    try {
+      LifecycleHelper.initAndStart(this);
+      getKeys();
+    } finally {
+      LifecycleHelper.stopAndClose(this);
+    }
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void testDefaultClear() throws Exception {
+    try {
+      LifecycleHelper.initAndStart(this);
+      clear();
+    } finally {
+      LifecycleHelper.stopAndClose(this);
+    }
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void testDefaultSize() throws Exception {
+    try {
+      LifecycleHelper.initAndStart(this);
+      size();
+    } finally {
+      LifecycleHelper.stopAndClose(this);
+    }
+  }
+
+  @Override
+  public void put(String key, Serializable value) throws CoreException {
+  }
+
+  @Override
+  public Object get(String key) throws CoreException {
+    return null;
+  }
+
+  @Override
+  public void remove(String key) throws CoreException {
+  }
+
+}
