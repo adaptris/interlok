1d6c31390e89cf38cbb9e9091593f6b23ec7dce4
Lewin Chan <lewin.chan@adaptris.com>
2019-03-14 16:49:33 +0000

    INTERLOK-2681 Switch ReadFile to use FsHelper#toFile instead.
    - Since we use the various FsWorker methods to check filestatus
    we have less "branches" required for coverage.

--- interlok-core/src/main/java/com/adaptris/core/services/ReadFileService.java
+++ interlok-core/src/main/java/com/adaptris/core/services/ReadFileService.java
@@ -1,17 +1,17 @@
 package com.adaptris.core.services;
 
+import static com.adaptris.fs.FsWorker.checkReadable;
+import static com.adaptris.fs.FsWorker.isFile;
+import static org.apache.commons.io.IOUtils.copy;
+import static org.apache.commons.lang3.StringUtils.defaultIfBlank;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
-
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-
-import org.apache.commons.lang3.StringUtils;
-
+import org.hibernate.validator.constraints.NotBlank;
 import com.adaptris.annotation.AdapterComponent;
 import com.adaptris.annotation.AdvancedConfig;
 import com.adaptris.annotation.AffectsMetadata;
@@ -22,9 +22,9 @@ import com.adaptris.core.AdaptrisMessage;
 import com.adaptris.core.CoreException;
 import com.adaptris.core.ServiceException;
 import com.adaptris.core.ServiceImp;
+import com.adaptris.core.fs.FsHelper;
 import com.adaptris.core.util.Args;
 import com.adaptris.core.util.ExceptionHelper;
-import com.adaptris.util.stream.StreamUtil;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 
 /**
@@ -33,130 +33,100 @@ import com.thoughtworks.xstream.annotations.XStreamAlias;
  * @config read-file-service
  */
 @AdapterComponent
-@ComponentProfile(summary = "Read a file from a specific path into the message payload", tag = "service,read,file")
+@ComponentProfile(summary = "Read a file from a specific path into the message payload",
+    tag = "service,file")
 @XStreamAlias("read-file-service")
-public class ReadFileService extends ServiceImp
-{
-	private static final String ERROR = "File Path has not been set, this service will not execute.";
+public class ReadFileService extends ServiceImp {
 
-	/**
-	 * The parameter for the path to the file to read.
-	 */
-	@NotNull
-	@Valid
-	@InputFieldHint(expression = true)
-	private String filePath;
+  /**
+   * The parameter for the path to the file to read.
+   */
+  @NotBlank
+  @InputFieldHint(expression = true)
+  private String filePath;
 
-	@AffectsMetadata
-	@AdvancedConfig
-	@InputFieldDefault(value = "null")
-	private String contentTypeMetadataKey;
+  @AffectsMetadata
+  @AdvancedConfig
+  @InputFieldDefault(value = "null")
+  private String contentTypeMetadataKey;
 
-	@AdvancedConfig
-	@Valid
-	@InputFieldDefault(value = "Files.probeContentType(Path)")
-	private ContentTypeProbe contentTypeProbe;
+  @AdvancedConfig
+  @Valid
+  @InputFieldDefault(value = "Files.probeContentType(Path)")
+  private ContentTypeProbe contentTypeProbe;
 
-	@Override
-	public void doService(final AdaptrisMessage message) throws ServiceException
-	{
-		try
-		{
-			final File file = new File(message.resolve(getFilePath()));
-			if (file.exists() && file.isFile())
-			{
-				log.info("Reading file : {}", file.getAbsolutePath());
-        try (FileInputStream in = new FileInputStream(file); OutputStream out = message.getOutputStream()) {
-          StreamUtil.copyAndClose(in, out);
-          if (StringUtils.isNotBlank(getContentTypeMetadataKey())) {
-            message.addMetadata(getContentTypeMetadataKey(), probeContentType(file));
-          }
-        }
-			}
-			else
-			{
-				log.error("File path does not exist or is a directory : {}", file.getAbsolutePath());
-				throw new FileNotFoundException("File " + file.getAbsolutePath() + " does not exist or is a directory!");
-			}
-		}
-		catch (final IOException e)
-		{
-		  throw ExceptionHelper.wrapServiceException(e);
-		}
-	}
+  @Override
+  public void doService(final AdaptrisMessage message) throws ServiceException {
+    try {
+      final File file = isFile(checkReadable(FsHelper.toFile(message.resolve(getFilePath()))));
+      log.debug("Reading file : {}", file.getCanonicalPath());
+      try (FileInputStream in = new FileInputStream(file);
+          OutputStream out = message.getOutputStream()) {
+        copy(in, out);
+      }
+      if (isNotBlank(getContentTypeMetadataKey())) {
+        message.addMetadata(getContentTypeMetadataKey(), probeContentType(file));
+      }
+    } catch (Exception e) {
+      throw ExceptionHelper.wrapServiceException(e);
+    }
+  }
 
-	private String probeContentType(File file) throws IOException {
-	  return StringUtils.defaultIfBlank(contentTypeProbe().probeContentType(file), "");
-	}
+  private String probeContentType(File file) throws IOException {
+    return defaultIfBlank(contentTypeProbe().probeContentType(file), "");
+  }
 
-	/**
-	 * {@inheritDoc}.
-	 */
-	@Override
-	public void prepare() throws CoreException
-	{
-		/* empty method */
-	}
+  @Override
+  public void prepare() throws CoreException {
+    /* empty method */
+  }
 
-	/**
-	 * {@inheritDoc}.
-	 */
-	@Override
-	protected void closeService()
-	{
-		/* empty method */
-	}
+  @Override
+  protected void closeService() {
+    /* empty method */
+  }
 
-	/**
-	 * {@inheritDoc}.
-	 */
-	@Override
-	protected void initService() throws CoreException
-	{
-		try
-		{
-			Args.notNull(getFilePath(), "filePath");
-		}
-		catch (Exception e)
-		{
-			throw ExceptionHelper.wrapCoreException(e);
-		}
-	}
+  @Override
+  protected void initService() throws CoreException {
+    try {
+      Args.notBlank(getFilePath(), "filePath");
+    } catch (Exception e) {
+      throw ExceptionHelper.wrapCoreException(e);
+    }
+  }
 
-	/**
-	 * Get the file path parameter.
-	 * 
-	 * @return The file path parameter.
-	 */
-	public String getFilePath()
-	{
-		return filePath;
-	}
+  /**
+   * Get the file path parameter.
+   * 
+   * @return The file path parameter.
+   */
+  public String getFilePath() {
+    return filePath;
+  }
 
-	/**
-	 * Set the file path parameter.
-	 * 
-	 * @param filePath
-	 *            The file path parameter.
-	 */
-	public void setFilePath(final String filePath)
-	{
-		this.filePath = Args.notBlank(filePath, "filePath");
-	}
+  /**
+   * Set the file path parameter.
+   * 
+   * @param filePath The file path parameter.
+   */
+  public void setFilePath(final String filePath) {
+    this.filePath = Args.notBlank(filePath, "filePath");
+  }
 
-	public String getContentTypeMetadataKey() {
-		return contentTypeMetadataKey;
-	}
+  public String getContentTypeMetadataKey() {
+    return contentTypeMetadataKey;
+  }
 
-	/**
-	 * Sets the metadata key set the content type as, if not provided will not be set. (default: null)
-	 * @param contentTypeMetadataKey
-	 */
-	public void setContentTypeMetadataKey(String contentTypeMetadataKey) {
-		this.contentTypeMetadataKey =  Args.notBlank(contentTypeMetadataKey, "contentTypeMetadataKey");
-	}
-	
-	 
+  /**
+   * Sets the metadata key set the content type as, if not provided will not be set. (default: null)
+   * 
+   * @param contentTypeMetadataKey
+   */
+  public void setContentTypeMetadataKey(String contentTypeMetadataKey) {
+    this.contentTypeMetadataKey = contentTypeMetadataKey;
+  }
+
+
   public ContentTypeProbe getContentTypeProbe() {
     return contentTypeProbe;
   }
@@ -164,13 +134,15 @@ public class ReadFileService extends ServiceImp
   public void setContentTypeProbe(ContentTypeProbe contentTypeProbe) {
     this.contentTypeProbe = contentTypeProbe;
   }
-  
+
   protected ContentTypeProbe contentTypeProbe() {
-    return getContentTypeProbe() != null ? getContentTypeProbe() : e -> { return Files.probeContentType(e.toPath()); };
+    return getContentTypeProbe() != null ? getContentTypeProbe() : e -> {
+      return Files.probeContentType(e.toPath());
+    };
   }
 
   @FunctionalInterface
-	public interface ContentTypeProbe {
-	  String probeContentType(File f) throws IOException ;
-	}
+  public interface ContentTypeProbe {
+    String probeContentType(File f) throws IOException;
+  }
 }
--- interlok-core/src/test/java/com/adaptris/core/services/ReadFileServiceTest.java
+++ interlok-core/src/test/java/com/adaptris/core/services/ReadFileServiceTest.java
@@ -1,108 +1,97 @@
 /*
  * Copyright 2015 Adaptris Ltd.
  * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package com.adaptris.core.services;
 
 import static org.junit.Assert.assertArrayEquals;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.nio.file.Files;
-
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
-
 import com.adaptris.core.AdaptrisMessage;
 import com.adaptris.core.AdaptrisMessageFactory;
 import com.adaptris.core.CoreException;
 import com.adaptris.core.GeneralServiceExample;
 
-public class ReadFileServiceTest extends GeneralServiceExample
-{
-	private static final String FILE = "build.gradle";
+public class ReadFileServiceTest extends GeneralServiceExample {
+  private static final String FILE = "build.gradle";
 
-	public ReadFileServiceTest(final String testName)
-	{
-		super(testName);
-	}
 
-	@Override
-	protected void setUp() throws Exception
-	{
-		/* empty method */
-	}
+
+  @Override
+  protected void setUp() throws Exception {
+    /* empty method */
+  }
 
 
   @Test
   public void testProbeContentType() throws Exception {
     final ReadFileService service = new ReadFileService();
     assertNotNull(service.contentTypeProbe());
-    ReadFileService.ContentTypeProbe myProbe = e-> { return Files.probeContentType(e.toPath()); };
+    ReadFileService.ContentTypeProbe myProbe = e -> {
+      return Files.probeContentType(e.toPath());
+    };
     service.setContentTypeProbe(myProbe);
     assertEquals(myProbe, service.getContentTypeProbe());
     assertEquals(myProbe, service.contentTypeProbe());
   }
-  
-	@Test
-	public void testService() throws Exception
-	{
-		final AdaptrisMessage message = AdaptrisMessageFactory.getDefaultInstance().newMessage();
-		final ReadFileService service = retrieveObjectForSampleConfig();
 
-		execute(service, message);
+  @Test
+  public void testService() throws Exception {
+    final AdaptrisMessage message = AdaptrisMessageFactory.getDefaultInstance().newMessage();
+    final ReadFileService service = new ReadFileService();
+    service.setFilePath(FILE);
 
-		final byte[] actual = message.getPayload();
+    execute(service, message);
 
-		final File file = new File(FILE);
-		final byte[] expected = new byte[(int)file.length()];
-		try (final FileInputStream fir = new FileInputStream(file))
-		{
-			fir.read(expected);
-		}
+    final byte[] actual = message.getPayload();
 
-		assertArrayEquals(expected, actual);
-	}
+    final File file = new File(FILE);
+    final byte[] expected = new byte[(int) file.length()];
+    try (final FileInputStream fir = new FileInputStream(file)) {
+      fir.read(expected);
+    }
 
-	 
-	@Test
-	public void testServiceContentType() throws Exception
-	{
-		final AdaptrisMessage message = AdaptrisMessageFactory.getDefaultInstance().newMessage();
-		final ReadFileService service = new ReadFileService();
-		service.setFilePath(PROPERTIES.getProperty("XmlTransformService.outputTestMessage"));
-		service.setContentTypeMetadataKey("contentType");
+    assertArrayEquals(expected, actual);
+  }
 
-		execute(service, message);
 
-		final byte[] actual = message.getPayload();
+  @Test
+  public void testServiceContentType() throws Exception {
+    final AdaptrisMessage message = AdaptrisMessageFactory.getDefaultInstance().newMessage();
+    final ReadFileService service = new ReadFileService();
+    service.setFilePath(PROPERTIES.getProperty("XmlTransformService.outputTestMessage"));
+    service.setContentTypeMetadataKey("contentType");
 
-		final File file = new File(PROPERTIES.getProperty("XmlTransformService.outputTestMessage"));
-		final byte[] expected = new byte[(int)file.length()];
-		try (final FileInputStream fir = new FileInputStream(file))
-		{
-			fir.read(expected);
-		}
+    execute(service, message);
 
-		assertArrayEquals(expected, actual);
-		assertTrue(message.containsKey("contentType"));
-		// Macs don't seem to be able to probe content type by default.
-		if (StringUtils.isNotBlank(message.getMetadataValue("contentType"))) {
-		  assertTrue(message.getMetadataValue("contentType").endsWith("/xml"));
-		}
-	}
+    final byte[] actual = message.getPayload();
+
+    final File file = new File(PROPERTIES.getProperty("XmlTransformService.outputTestMessage"));
+    final byte[] expected = new byte[(int) file.length()];
+    try (final FileInputStream fir = new FileInputStream(file)) {
+      fir.read(expected);
+    }
+
+    assertArrayEquals(expected, actual);
+    assertTrue(message.containsKey("contentType"));
+    // Macs don't seem to be able to probe content type by default.
+    if (StringUtils.isNotBlank(message.getMetadataValue("contentType"))) {
+      assertTrue(message.getMetadataValue("contentType").endsWith("/xml"));
+    }
+  }
 
   @Test
   public void testService_NoContentType() throws Exception {
@@ -118,29 +107,24 @@ public class ReadFileServiceTest extends GeneralServiceExample
     }
     assertArrayEquals(expected, actual);
   }
-  
-	@Test
-	public void testServiceFailedInit() throws Exception
-	{
-		try
-		{
-			final AdaptrisMessage message = AdaptrisMessageFactory.getDefaultInstance().newMessage();
-			final ReadFileService service = new ReadFileService();
 
-			execute(service, message);
-			fail();
-		}
-    catch (@SuppressWarnings("unused") final CoreException expected)
-		{
-			/* expected result */
-		}
-	}
+  @Test
+  public void testServiceFailedInit() throws Exception {
+    try {
+      final AdaptrisMessage message = AdaptrisMessageFactory.getDefaultInstance().newMessage();
+      final ReadFileService service = new ReadFileService();
 
-	@Override
-	protected ReadFileService retrieveObjectForSampleConfig()
-	{
-		final ReadFileService service = new ReadFileService();
-    service.setFilePath(FILE);
-		return service;
-	}
+      execute(service, message);
+      fail();
+    } catch (@SuppressWarnings("unused") final CoreException expected) {
+      /* expected result */
+    }
+  }
+
+  @Override
+  protected ReadFileService retrieveObjectForSampleConfig() {
+    final ReadFileService service = new ReadFileService();
+    service.setFilePath("%message{pathToFile}, or a file path");
+    return service;
+  }
 }
