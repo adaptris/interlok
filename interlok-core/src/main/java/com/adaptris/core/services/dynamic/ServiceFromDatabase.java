cf749ec37784dd2fb0baba94ec3de2880fb3b97f
Lewin Chan <lewin.chan@adaptris.com>
2019-03-21 14:37:02 +0000

    INTERLOK-2702 Add a service-extractor impl for databases.
    Add service-extractor implementation to extract dynamic
    services from a database.
    It could just be done with JdbcDataQueryService

---
+++ interlok-core/src/main/java/com/adaptris/core/services/dynamic/ServiceFromDatabase.java
@@ -0,0 +1,137 @@
+/*******************************************************************************
+ * Copyright 2019 Adaptris Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package com.adaptris.core.services.dynamic;
+
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.NotBlank;
+import com.adaptris.annotation.ComponentProfile;
+import com.adaptris.annotation.DisplayOrder;
+import com.adaptris.annotation.InputFieldHint;
+import com.adaptris.core.AdaptrisConnection;
+import com.adaptris.core.AdaptrisMessage;
+import com.adaptris.core.ServiceException;
+import com.adaptris.core.jdbc.DatabaseConnection;
+import com.adaptris.core.services.jdbc.FirstRowMetadataTranslator;
+import com.adaptris.core.services.jdbc.JdbcDataQueryService;
+import com.adaptris.core.util.Args;
+import com.adaptris.core.util.JdbcUtil;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+/**
+ * Extract the service to execute from a database
+ * 
+ * <p>
+ * This executes the configured query, takes the first column of the first ResultSet and uses that
+ * as the source for the dynamic service.
+ * </p>
+ * <p>
+ * Since it supports the expression syntax; this is perfectly acceptable; It is up to you to protect
+ * against SQL injection attacks.
+ * 
+ * <pre>
+ * {@code SELECT dynamicService FROM services 
+ *        WHERE src='%message{source}' 
+ *              AND dest='%message{destination}' 
+ *              AND msgType='%message{messageType}'}
+ * </pre>
+ * </p>
+ * <p>
+ * The alternative to this would be to use {@link JdbcDataQueryService} with a
+ * {@link FirstRowMetadataTranslator} and subsequently a {@link ServiceFromDataInputParameter} (from
+ * metadata). That might be more performant as you would benefit from prepared statement caching and
+ * mitigate against SQL injection style attacks.
+ * </p>
+ * 
+ * @config dynamic-service-from-database
+ * @see DynamicServiceExecutor
+ * 
+ */
+@XStreamAlias("dynamic-service-from-database")
+@ComponentProfile(summary = "Extract the service to execute from a database",
+    recommended = {DatabaseConnection.class})
+@DisplayOrder(order = {"key", "connection"})
+public class ServiceFromDatabase extends ExtractorWithConnection {
+
+  @NotBlank
+  @InputFieldHint(expression = true)
+  private String query;
+  @NotNull
+  @Valid
+  private AdaptrisConnection connection;
+
+  public ServiceFromDatabase() {
+
+  }
+
+  @Override
+  public InputStream getInputStream(AdaptrisMessage m) throws Exception {
+    Connection jdbcCon = getConnection().retrieveConnection(DatabaseConnection.class).connect();
+    String jdbcQuery = m.resolve(getQuery());
+    ResultSet resultSet = null;
+    Statement statement = null;
+    InputStream result = null;
+    try {
+      statement = jdbcCon.createStatement();
+      resultSet = statement.executeQuery(jdbcQuery);
+      if (resultSet.next()) {
+        result = new ServiceInputStream(resultSet, statement);
+      } else {
+        throw new ServiceException("No results from [" + jdbcQuery + "]");
+      }
+    } catch (Exception e) {
+      JdbcUtil.closeQuietly(resultSet, statement);
+      throw e;
+    }
+    return result;
+  }
+
+  public String getQuery() {
+    return query;
+  }
+
+  public void setQuery(String query) {
+    this.query = Args.notBlank(query, "query");
+  }
+
+  public ServiceFromDatabase withQuery(String q) {
+    setQuery(q);
+    return this;
+  }
+
+  private class ServiceInputStream extends FilterInputStream {
+    private ResultSet result;
+    private Statement statement;
+
+    ServiceInputStream(ResultSet rs, Statement stmt) throws Exception {
+      super(rs.getAsciiStream(1));
+      result = rs;
+      statement = stmt;
+    }
+
+    @Override
+    public void close() throws IOException {
+      super.close();
+      JdbcUtil.closeQuietly(result, statement);
+    }
+  }
+}
---
+++ interlok-core/src/test/java/com/adaptris/core/services/dynamic/ServiceFromDatabaseTest.java
@@ -0,0 +1,155 @@
+/*******************************************************************************
+ * Copyright 2019 Adaptris Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package com.adaptris.core.services.dynamic;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+import com.adaptris.core.AdaptrisMessage;
+import com.adaptris.core.AdaptrisMessageFactory;
+import com.adaptris.core.DefaultMarshaller;
+import com.adaptris.core.ServiceException;
+import com.adaptris.core.ServiceList;
+import com.adaptris.core.jdbc.JdbcConnection;
+import com.adaptris.core.services.LogMessageService;
+import com.adaptris.core.util.LifecycleHelper;
+
+public class ServiceFromDatabaseTest {
+
+  private static final String JDBC_DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
+  private static final String JDBC_URL = "jdbc:derby:memory:DSL_FROM_DB;create=true";
+  private static final String TABLE_NAME = "services";
+  private static final String DROP_STMT = String.format("DROP TABLE %s", TABLE_NAME);
+  private static final String CREATE_STMT = String.format(
+      "CREATE TABLE %s (source VARCHAR(128) NOT NULL, "
+      + "destination VARCHAR(128) NOT NULL, "
+          + "dynamicService CLOB NOT NULL)",
+      TABLE_NAME);
+  private static final String INSERT_STMT = String.format(
+      "INSERT INTO %s (source, destination, dynamicService)" + "values (?,?,?)", TABLE_NAME);
+  private static final String SELECT_STMT = String.format(
+      "SELECT dynamicService FROM %s WHERE source='%%message{source}' AND destination='%%message{destination}'",
+      TABLE_NAME);
+
+
+  @Test
+  public void testGetInputStream() throws Exception {
+    DatabaseEntry entry = new DatabaseEntry("mySourcePartner", "myDestinationPartner");
+    createDatabase(Arrays.asList(entry));
+    JdbcConnection jdbcConn = configure(new JdbcConnection());
+
+    ServiceFromDatabase extractor =
+        new ServiceFromDatabase().withQuery(SELECT_STMT).withConnection(jdbcConn);
+    
+    AdaptrisMessage msg = AdaptrisMessageFactory.getDefaultInstance().newMessage();
+    msg.addMetadata("source", "mySourcePartner");
+    msg.addMetadata("destination", "myDestinationPartner");
+    try {
+      LifecycleHelper.initAndStart(extractor);
+      try (InputStream in = extractor.getInputStream(msg)) {
+        assertNotNull(in);
+        assertEquals(ServiceList.class,
+            DefaultMarshaller.getDefaultMarshaller().unmarshal(in).getClass());
+      }
+    } finally {
+      LifecycleHelper.stopAndClose(extractor);
+    }
+  }
+
+  @Test(expected = ServiceException.class)
+  public void testNotFound() throws Exception {
+    DatabaseEntry entry = new DatabaseEntry("mySourcePartner", "myDestinationPartner");
+    createDatabase(Arrays.asList(entry));
+    JdbcConnection jdbcConn = configure(new JdbcConnection());
+    ServiceFromDatabase extractor =
+        new ServiceFromDatabase().withQuery(SELECT_STMT).withConnection(jdbcConn);
+    AdaptrisMessage msg = AdaptrisMessageFactory.getDefaultInstance().newMessage();
+    msg.addMetadata("source", "anotherSourcePartner");
+    msg.addMetadata("destination", "anotherDestinationPartner");
+    try {
+      LifecycleHelper.initAndStart(extractor);
+      // Since there's no resultSet.next(), should throw a ServiceException.
+      InputStream in = extractor.getInputStream(msg);
+    } finally {
+      LifecycleHelper.stopAndClose(extractor);
+    }
+  }
+
+
+  protected static Connection createConnection() throws Exception {
+    Class.forName(JDBC_DRIVER);
+    Connection c = DriverManager.getConnection(JDBC_URL);
+    c.setAutoCommit(true);
+    return c;
+  }
+
+  protected static JdbcConnection configure(JdbcConnection c) {
+    c.setDriverImp(JDBC_DRIVER);
+    c.setConnectUrl(JDBC_URL);
+    c.setAutoCommit(true);
+    return c;
+  }
+
+  protected static void createDatabase(List<DatabaseEntry> list) throws Exception {
+    createDatabase();
+    try (Connection c = createConnection();
+        PreparedStatement insert = c.prepareStatement(INSERT_STMT);) {
+      for (DatabaseEntry entry : list) {
+        insert.clearParameters();
+        insert.setString(1, entry.source);
+        insert.setString(2, entry.destination);
+        insert.setString(3, entry.service);
+        insert.executeUpdate();
+      }
+    }
+  }
+
+  protected static void createDatabase() throws Exception {
+    try (Connection c = createConnection(); Statement s = c.createStatement()) {
+      executeQuietly(s, DROP_STMT);
+      s.execute(CREATE_STMT);
+    }
+  }
+
+  protected static void executeQuietly(Statement s, String sql) {
+    try {
+      s.execute(sql);
+    } catch (Exception e) {
+      ;
+    }
+  }
+
+
+  private class DatabaseEntry {
+    String source;
+    String destination;
+    String service;
+
+    public DatabaseEntry(String src, String dest) throws Exception {
+      source = src;
+      destination = dest;
+      service = DynamicServiceExecutorTest.createMessage(new ServiceList(new LogMessageService()))
+          .getContent();
+    }
+  }
+}
