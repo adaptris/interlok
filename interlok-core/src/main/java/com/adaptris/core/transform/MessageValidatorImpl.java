0d893068fcdfb2e19ce9d9d47c08bb522e610a23
Lewin Chan <lewin.chan@adaptris.com>
2019-05-09 11:59:38 +0100

    Made schema-cache default to 'null' and apply a default on lifecycle. Since it is a nested object; it reduces clutter in the UI.

--- interlok-core/src/main/java/com/adaptris/core/transform/MessageValidatorImpl.java
+++ interlok-core/src/main/java/com/adaptris/core/transform/MessageValidatorImpl.java
@@ -18,6 +18,7 @@ package com.adaptris.core.transform;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import com.adaptris.core.CoreException;
 
 public abstract class MessageValidatorImpl implements MessageValidator {
@@ -25,26 +26,6 @@ public abstract class MessageValidatorImpl implements MessageValidator {
   protected transient Logger log = LoggerFactory.getLogger(this.getClass().getName());
 
   @Override
-  public void prepare() throws CoreException {
-  }
-
-
-  @Override
-  public void init() throws CoreException {
-  }
-
-  @Override
-  public void start() throws CoreException {
-  }
-
-  @Override
-  public void stop() {
-
-  }
-
-  @Override
-  public void close() {
-
-  }
+  public void prepare() throws CoreException {}
 
 }
