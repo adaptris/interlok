5d86fec3aab44d9cb8b0c2435ba0b76a074b65f4
Lewin Chan <lewin.chan@adaptris.com>
2019-10-09 10:51:22 +0100

    INTERLOK-2860 Fix javadocs, add aliases
    @mcwarman had a few issues testing other PR's around the docs.

--- interlok-common/src/main/java/com/adaptris/interlok/cloud/RemoteBlobFilterWrapper.java
+++ interlok-common/src/main/java/com/adaptris/interlok/cloud/RemoteBlobFilterWrapper.java
@@ -4,20 +4,34 @@ import static org.apache.commons.lang3.StringUtils.isEmpty;
 import java.io.FileFilter;
 import java.lang.reflect.Constructor;
 import javax.validation.constraints.NotBlank;
+import com.adaptris.annotation.ComponentProfile;
 import com.adaptris.interlok.util.Args;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
 
 /**
  * Wraps a {@link FileFilter} instance and allows you to use that as your filter.
  * <p>
- * The file filter implementation is configured as a classname, and is expected to have a String constructor (the filter-expression)
- * is used as that parameter; this is because not all FileFilter implementations will have aliases and configuration that are
- * appropriate for marshalling.
+ * The file filter implementation is configured as a classname, and is expected to have a
+ * <strong>String constructor</strong> (the filter-expression is used as that parameter); this is
+ * because not all FileFilter implementations will have aliases and configuration that are
+ * appropriate for marshalling
  * </p>
  * <p>
- * Since a {@link RemoteBlob} does not expose possible methods of {@link java.io.File}, it is wrapped as a {@link RemoteFile}. If
- * your filter uses anything other than the filename / size / lastmodified, then results will be undefined.
+ * Apache commons-io filters such as {@code SizeBasedFilter} only provide a numeric constructor, so
+ * these cannot yet be supported via this wrapper; however, you have other options available in the
+ * {@code com.adaptris.core.fs} package.
  * </p>
+ * <p>
+ * Since a {@link RemoteBlob} does not expose all the possible methods of {@link java.io.File}, it
+ * is wrapped as a {@link RemoteFile}. <strong>If your filter uses anything other than the filename
+ * / size / lastmodified, then results will be undefined.</strong>
+ * </p>
+ * 
+ * 
+ * @config remote-blob-filter-wrapper
  */
+@XStreamAlias("remote-blob-filter-wrapper")
+@ComponentProfile(summary = "Wraps a FileFilter instance for filtering remote blobs", since = "3.9.2")
 public class RemoteBlobFilterWrapper implements RemoteBlobFilter {
 
   @NotBlank
