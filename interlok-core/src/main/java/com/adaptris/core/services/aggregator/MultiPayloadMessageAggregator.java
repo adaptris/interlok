83abf540e1ac9de820bbbf725fe66ee4367123ea
Ashley Anderson <ashley.anderson@reedbusiness.com>
2019-12-10 15:22:03 +0000

    INTERLOK-3088 Add replace/append option
    When merging the messages into the multi-payload message, allow the user
    to decide whether the original payload is replaced (the default) or
    whether the new payloads are appended to the list.

--- interlok-core/src/main/java/com/adaptris/core/services/aggregator/MultiPayloadMessageAggregator.java
+++ interlok-core/src/main/java/com/adaptris/core/services/aggregator/MultiPayloadMessageAggregator.java
@@ -1,12 +1,16 @@
 package com.adaptris.core.services.aggregator;
 
+import com.adaptris.annotation.AdvancedConfig;
 import com.adaptris.core.AdaptrisMessage;
 import com.adaptris.core.CoreException;
 import com.adaptris.core.MultiPayloadAdaptrisMessage;
 import com.adaptris.core.ServiceException;
+import com.adaptris.core.util.Args;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.util.Collection;
 
 /**
@@ -16,6 +20,39 @@ import java.util.Collection;
 public class MultiPayloadMessageAggregator extends MessageAggregatorImpl
 {
 	private static final transient Logger log = LoggerFactory.getLogger(MultiPayloadMessageAggregator.class);
+
+	@NotNull
+	@Valid
+	@AdvancedConfig
+	private Boolean replaceOriginalMessage = true;
+
+	/**
+	 * Set whether to replace the original multi-payload message
+	 * payload. If true then the original message will only
+	 * contain the payloads from the collection of messages,
+	 * otherwise it will append the collection of messages while
+	 * maintaining the original message payload.
+	 *
+	 * @param replaceOriginalMessage Whether to replace the original message payload.
+	 */
+	public void setReplaceOriginalMessage(Boolean replaceOriginalMessage)
+	{
+		this.replaceOriginalMessage = Args.notNull(replaceOriginalMessage, "replaceOriginalMessage");
+	}
+
+	/**
+	 * Get whether to replace the original multi-payload message
+	 * payload. If true then the original message will only
+	 * contain the payloads from the collection of messages,
+	 * otherwise it will append the collection of messages while
+	 * maintaining the original message payload.
+	 *
+	 * @return Whether to replace the original message payload.
+	 */
+	public Boolean getReplaceOriginalMessage()
+	{
+		return replaceOriginalMessage;
+	}
 
 	/**
 	 * Joins multiple {@link AdaptrisMessage}s into a single MultiPayloadAdaptrisMessage object.
@@ -35,11 +72,16 @@ public class MultiPayloadMessageAggregator extends MessageAggregatorImpl
 		log.info("Adding messages to existing message [{}]", original.getUniqueId());
 
 		MultiPayloadAdaptrisMessage multiMessage = (MultiPayloadAdaptrisMessage)original;
+		String originalId = multiMessage.getCurrentPayloadId();
 		for (AdaptrisMessage message : messages)
 		{
 			log.info("Adding message payload [{}]", message.getUniqueId());
 
 			multiMessage.addPayload(message.getUniqueId(), message.getPayload());
+		}
+		if (replaceOriginalMessage)
+		{
+			multiMessage.deletePayload(originalId);
 		}
 
 		log.info("Finished adding {} messages", messages.size());
