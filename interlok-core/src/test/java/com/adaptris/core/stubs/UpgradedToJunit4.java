bf6395a07d1836bbc354d2d24c8c48937f43377b
Lewin Chan <lewin.chan@adaptris.com>
2020-01-13 09:35:32 +0000

    Remove default method to force compile failures

--- interlok-core/src/test/java/com/adaptris/core/stubs/UpgradedToJunit4.java
+++ interlok-core/src/test/java/com/adaptris/core/stubs/UpgradedToJunit4.java
@@ -2,8 +2,5 @@ package com.adaptris.core.stubs;
 
 public interface UpgradedToJunit4 {
 
-  // For concrete implementations to override
-  default boolean isAnnotatedForJunit4() {
-    return false;
-  }
+  boolean isAnnotatedForJunit4();
 }
