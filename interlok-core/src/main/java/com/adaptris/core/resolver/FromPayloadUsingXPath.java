fc84811a6e9d24d3e8151405da022100b6f64604
Ashley Morgan Anderson <ashley.anderson@reedbusiness.com>
2020-01-24 11:58:51 +0000

    Remove imports and switch to the abomination that is 2 space indentation

--- interlok-core/src/main/java/com/adaptris/core/resolver/FromPayloadUsingXPath.java
+++ interlok-core/src/main/java/com/adaptris/core/resolver/FromPayloadUsingXPath.java
@@ -1,111 +1,1 @@
-package com.adaptris.core.resolver;
-
-import com.adaptris.core.util.DocumentBuilderFactoryBuilder;
-import com.adaptris.interlok.resolver.ResolverImp;
-import com.adaptris.interlok.resolver.UnresolvableException;
-import com.adaptris.util.KeyValuePair;
-import com.adaptris.util.KeyValuePairSet;
-import com.adaptris.util.text.xml.SimpleNamespaceContext;
-import com.adaptris.util.text.xml.XPath;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
-import javax.xml.XMLConstants;
-import javax.xml.namespace.NamespaceContext;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpression;
-import java.io.ByteArrayInputStream;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-public class FromPayloadUsingXPath extends ResolverImp
-{
-	private static final String RESOLVE_PAYLOAD_REGEXP = "^.*%payload\\{xpath:([\\w!\\$\"#&%'\\*\\+,\\-\\.:=\\(\\)\\[\\]\\/@\\|]+)\\}.*$";
-	private static final transient Pattern PAYLOAD_RESOLVER = Pattern.compile(RESOLVE_PAYLOAD_REGEXP);
-
-	private final DocumentBuilderFactoryBuilder factoryBuilder;
-	private final NamespaceContext namespaceContext;
-
-	public FromPayloadUsingXPath()
-	{
-		factoryBuilder = DocumentBuilderFactoryBuilder.newRestrictedInstance();
-		KeyValuePairSet result = new KeyValuePairSet();
-		result.add(new KeyValuePair("xsd", XMLConstants.W3C_XML_SCHEMA_NS_URI));
-		result.add(new KeyValuePair("xs", XMLConstants.W3C_XML_SCHEMA_NS_URI));
-		result.add(new KeyValuePair(XMLConstants.XML_NS_PREFIX, XMLConstants.XML_NS_URI));
-		result.add(new KeyValuePair(XMLConstants.XMLNS_ATTRIBUTE, XMLConstants.XMLNS_ATTRIBUTE_NS_URI));
-		namespaceContext = SimpleNamespaceContext.create(result);
-	}
-
-	@Override
-	public String resolve(String value)
-	{
-		throw new UnresolvableException();
-	}
-
-	/**
-	 * Attempt to resolve a value externally.
-	 *
-	 * @param value The string to resolve.
-	 * @param target The document to search.
-	 *
-	 * @return The resolved value.
-	 */
-	@Override
-	public String resolve(String value, String target)
-	{
-		Document document;
-		try
-		{
-			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder builder = factoryBuilder.newDocumentBuilder(factory);
-			document = builder.parse(new ByteArrayInputStream(target.getBytes()));
-		}
-		catch (Exception e)
-		{
-			log.error("Could not parse XML document for resolve", e);
-			throw new UnresolvableException(e);
-		}
-
-		Matcher m = PAYLOAD_RESOLVER.matcher(value);
-		while (m.matches())
-		{
-			String path = m.group(1);
-			String replaceWith = extract(path, document);
-			log.info("XPath {} found {}", path, replaceWith);
-			value = value.replace("%payload{xpath:" + path + "}", replaceWith);
-			m = PAYLOAD_RESOLVER.matcher(value);
-		}
-		return value;
-	}
-
-	private String extract(String path, Document document)
-	{
-		try
-		{
-			XPath xPath = XPath.newXPathInstance(factoryBuilder, namespaceContext);
-			return xPath.selectSingleTextItem(document, path);
-		}
-		catch (Exception e)
-		{
-			log.error("Could not use XPath {} to extract data from message payload", path, e);
-			throw new UnresolvableException("Could not use XPath {} to extract data from message payload");
-		}
-	}
-
-	/**
-	 * Can this resolver handle this type of value.
-	 *
-	 * @param value the value e.g. {@code %payload{xpath:…}}
-	 *
-	 * @return True if the value will provide matches, false otherwise.
-	 */
-	@Override
-	public boolean canHandle(String value)
-	{
-		return PAYLOAD_RESOLVER.matcher(value).matches();
-	}
-}
+package com.adaptris.core.resolver;import com.adaptris.core.util.DocumentBuilderFactoryBuilder;import com.adaptris.interlok.resolver.ResolverImp;import com.adaptris.interlok.resolver.UnresolvableException;import com.adaptris.util.KeyValuePair;import com.adaptris.util.KeyValuePairSet;import com.adaptris.util.text.xml.SimpleNamespaceContext;import com.adaptris.util.text.xml.XPath;import org.w3c.dom.Document;import javax.xml.XMLConstants;import javax.xml.namespace.NamespaceContext;import javax.xml.parsers.DocumentBuilder;import javax.xml.parsers.DocumentBuilderFactory;import java.io.ByteArrayInputStream;import java.util.regex.Matcher;import java.util.regex.Pattern;public class FromPayloadUsingXPath extends ResolverImp{  private static final String RESOLVE_PAYLOAD_REGEXP = "^.*%payload\\{xpath:([\\w!\\$\"#&%'\\*\\+,\\-\\.:=\\(\\)\\[\\]\\/@\\|]+)\\}.*$";  private static final transient Pattern PAYLOAD_RESOLVER = Pattern.compile(RESOLVE_PAYLOAD_REGEXP);  private final DocumentBuilderFactoryBuilder factoryBuilder;  private final NamespaceContext namespaceContext;  public FromPayloadUsingXPath()  {    factoryBuilder = DocumentBuilderFactoryBuilder.newRestrictedInstance();    KeyValuePairSet result = new KeyValuePairSet();    result.add(new KeyValuePair("xsd", XMLConstants.W3C_XML_SCHEMA_NS_URI));    result.add(new KeyValuePair("xs", XMLConstants.W3C_XML_SCHEMA_NS_URI));    result.add(new KeyValuePair(XMLConstants.XML_NS_PREFIX, XMLConstants.XML_NS_URI));    result.add(new KeyValuePair(XMLConstants.XMLNS_ATTRIBUTE, XMLConstants.XMLNS_ATTRIBUTE_NS_URI));    namespaceContext = SimpleNamespaceContext.create(result);  }  @Override  public String resolve(String value)  {    throw new UnresolvableException();  }  /**   * Attempt to resolve a value externally.   *   * @param value The string to resolve.   * @param target The document to search.   *   * @return The resolved value.   */  @Override  public String resolve(String value, String target)  {    Document document;    try    {      DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();      DocumentBuilder builder = factoryBuilder.newDocumentBuilder(factory);      document = builder.parse(new ByteArrayInputStream(target.getBytes()));    }    catch (Exception e)    {      log.error("Could not parse XML document for resolve", e);      throw new UnresolvableException(e);    }    Matcher m = PAYLOAD_RESOLVER.matcher(value);    while (m.matches())    {      String path = m.group(1);      String replaceWith = extract(path, document);      log.info("XPath {} found {}", path, replaceWith);      value = value.replace("%payload{xpath:" + path + "}", replaceWith);      m = PAYLOAD_RESOLVER.matcher(value);    }    return value;  }  private String extract(String path, Document document)  {    try    {      XPath xPath = XPath.newXPathInstance(factoryBuilder, namespaceContext);      return xPath.selectSingleTextItem(document, path);    }    catch (Exception e)    {      log.error("Could not use XPath {} to extract data from message payload", path, e);      throw new UnresolvableException("Could not use XPath {} to extract data from message payload");    }  }  /**   * Can this resolver handle this type of value.   *   * @param value the value e.g. {@code %payload{xpath:…}}   *   * @return True if the value will provide matches, false otherwise.   */  @Override  public boolean canHandle(String value)  {    return PAYLOAD_RESOLVER.matcher(value).matches();  }}
--- interlok-core/src/test/java/com/adaptris/core/resolver/FromPayloadUsingXPathTest.java
+++ interlok-core/src/test/java/com/adaptris/core/resolver/FromPayloadUsingXPathTest.java
@@ -11,89 +11,89 @@ import static org.junit.Assert.fail;
 
 public class FromPayloadUsingXPathTest
 {
-	private static final String REGEX_POOR = "%payload{xpath:/something[0--]invalid}";
-	private static final String REGEX_MISS = "%payload{xpath:/text/para/quote/text()}";
-	static final String REGEX_BAD = "%payload{jsonpath:$['text']['para']['sent'][3]}";
-	static final String REGEX_GOOD = "%payload{xpath:/text/para/sent[3]/text()}";
-	static final String DATA = "<text><para><sent>Hipster ipsum dolor amet portland asymmetrical try-hard roof party poke, schlitz blue bottle pop-up 3 wolf moon kogi hammock kitsch austin health goth.</sent><sent>Ethical mlkshk crucifix pug, hexagon XOXO tote bag portland typewriter celiac cornhole lumbersexual 8-bit pop-up.</sent><sent>Cred typewriter seitan, narwhal quinoa master cleanse mlkshk freegan.</sent><sent>Whatever vape paleo, mustache taiyaki XOXO chia ethical viral.</sent></para></text>";
-	static final String RESULT = "Cred typewriter seitan, narwhal quinoa master cleanse mlkshk freegan.";
+  private static final String REGEX_POOR = "%payload{xpath:/something[0--]invalid}";
+  private static final String REGEX_MISS = "%payload{xpath:/text/para/quote/text()}";
+  static final String REGEX_BAD = "%payload{jsonpath:$['text']['para']['sent'][3]}";
+  static final String REGEX_GOOD = "%payload{xpath:/text/para/sent[3]/text()}";
+  static final String DATA = "<text><para><sent>Hipster ipsum dolor amet portland asymmetrical try-hard roof party poke, schlitz blue bottle pop-up 3 wolf moon kogi hammock kitsch austin health goth.</sent><sent>Ethical mlkshk crucifix pug, hexagon XOXO tote bag portland typewriter celiac cornhole lumbersexual 8-bit pop-up.</sent><sent>Cred typewriter seitan, narwhal quinoa master cleanse mlkshk freegan.</sent><sent>Whatever vape paleo, mustache taiyaki XOXO chia ethical viral.</sent></para></text>";
+  static final String RESULT = "Cred typewriter seitan, narwhal quinoa master cleanse mlkshk freegan.";
 
-	private FromPayloadUsingXPath resolver = new FromPayloadUsingXPath();
+  private FromPayloadUsingXPath resolver = new FromPayloadUsingXPath();
 
-	@Test
-	public void testCanHandle()
-	{
-		assertTrue(resolver.canHandle(REGEX_GOOD));
-		assertFalse(resolver.canHandle(REGEX_BAD));
-	}
+  @Test
+  public void testCanHandle()
+  {
+    assertTrue(resolver.canHandle(REGEX_GOOD));
+    assertFalse(resolver.canHandle(REGEX_BAD));
+  }
 
-	@Test
-	public void testResolveSuccess()
-	{
-		assertEquals(RESULT, resolver.resolve(REGEX_GOOD, DATA));
-	}
+  @Test
+  public void testResolveSuccess()
+  {
+    assertEquals(RESULT, resolver.resolve(REGEX_GOOD, DATA));
+  }
 
-	@Test
-	public void testResolveException()
-	{
-		try
-		{
-			resolver.resolve(REGEX_GOOD);
-			fail();
-		}
-		catch (Exception e)
-		{
-			// expected
-		}
-	}
+  @Test
+  public void testResolveException()
+  {
+    try
+    {
+      resolver.resolve(REGEX_GOOD);
+      fail();
+    }
+    catch (Exception e)
+    {
+      // expected
+    }
+  }
 
-	@Test
-	public void testResolveRegexNotFound()
-	{
-		try
-		{
-			assertEquals("", resolver.resolve(REGEX_MISS, DATA));
-		}
-		catch (Exception e)
-		{
-			// expected
-		}
-	}
+  @Test
+  public void testResolveRegexNotFound()
+  {
+    try
+    {
+      assertEquals("", resolver.resolve(REGEX_MISS, DATA));
+    }
+    catch (Exception e)
+    {
+      // expected
+    }
+  }
 
-	@Test
-	public void testNotXML()
-	{
-		try
-		{
-			resolver.resolve(REGEX_GOOD, RESULT);
-			fail();
-		}
-		catch (Exception e)
-		{
-			// expected
-		}
-	}
+  @Test
+  public void testNotXML()
+  {
+    try
+    {
+      resolver.resolve(REGEX_GOOD, RESULT);
+      fail();
+    }
+    catch (Exception e)
+    {
+      // expected
+    }
+  }
 
-	@Test
-	public void testResolveRegexInvalid()
-	{
-		assertEquals(REGEX_BAD, resolver.resolve(REGEX_BAD, DATA));
-		try
-		{
-			resolver.resolve(REGEX_POOR, DATA);
-			fail();
-		}
-		catch (Exception e)
-		{
-			// expected
-		}
-	}
+  @Test
+  public void testResolveRegexInvalid()
+  {
+    assertEquals(REGEX_BAD, resolver.resolve(REGEX_BAD, DATA));
+    try
+    {
+      resolver.resolve(REGEX_POOR, DATA);
+      fail();
+    }
+    catch (Exception e)
+    {
+      // expected
+    }
+  }
 
-	@Test
-	public void testExternalResolver()
-	{
-		assertNull(ExternalResolver.resolve(null, null));
-		assertEquals(FromPayloadUsingXPathTest.RESULT, ExternalResolver.resolve(FromPayloadUsingXPathTest.REGEX_GOOD, FromPayloadUsingXPathTest.DATA));
-		assertEquals(FromPayloadUsingXPathTest.REGEX_BAD, ExternalResolver.resolve(FromPayloadUsingXPathTest.REGEX_BAD, FromPayloadUsingXPathTest.DATA));
-	}
+  @Test
+  public void testExternalResolver()
+  {
+    assertNull(ExternalResolver.resolve(null, null));
+    assertEquals(FromPayloadUsingXPathTest.RESULT, ExternalResolver.resolve(FromPayloadUsingXPathTest.REGEX_GOOD, FromPayloadUsingXPathTest.DATA));
+    assertEquals(FromPayloadUsingXPathTest.REGEX_BAD, ExternalResolver.resolve(FromPayloadUsingXPathTest.REGEX_BAD, FromPayloadUsingXPathTest.DATA));
+  }
 }
