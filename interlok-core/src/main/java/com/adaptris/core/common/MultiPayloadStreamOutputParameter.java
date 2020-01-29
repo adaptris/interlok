1e3de0e7ca2c3f259c09c237d6be9ff1f463ee1b
Ashley Morgan Anderson <ashley.anderson@reedbusiness.com>
2020-01-29 19:34:53 +0000

    INTERLOK-3141 Add stream data output parameter class

---
+++ interlok-core/src/main/java/com/adaptris/core/common/MultiPayloadStreamOutputParameter.java
@@ -0,0 +1,90 @@
+package com.adaptris.core.common;
+
+import com.adaptris.core.MultiPayloadAdaptrisMessage;
+import com.adaptris.core.util.ExceptionHelper;
+import com.adaptris.interlok.InterlokException;
+import com.adaptris.interlok.types.InterlokMessage;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import static com.adaptris.util.stream.StreamUtil.copyAndClose;
+import static org.apache.commons.lang3.StringUtils.defaultIfEmpty;
+import static org.apache.commons.lang3.StringUtils.isEmpty;
+
+/**
+ * This {@code MultiPayloadDataOutputParameter} is used when you want to
+ * insert data into the {@link MultiPayloadAdaptrisMessage} payload.
+ *
+ * @author andersonam
+ * @config multi-payload-stream-output-parameter
+ */
+@XStreamAlias("multi-payload-stream-output-parameter")
+public class MultiPayloadStreamOutputParameter extends PayloadStreamOutputParameter implements MultiPayloadDataOutputParameter<InputStreamWithEncoding>
+{
+	private String payloadId;
+
+	/**
+	 * {@inheritDoc}.
+	 */
+	@Override
+	public String getPayloadId()
+	{
+		return payloadId;
+	}
+
+	/**
+	 * {@inheritDoc}.
+	 */
+	@Override
+	public void setPayloadId(String payloadId)
+	{
+		this.payloadId = payloadId;
+	}
+
+	/**
+	 * {@inheritDoc}.
+	 */
+	@Override
+	public void insert(InputStreamWithEncoding data, InterlokMessage m) throws InterlokException
+	{
+		if (m instanceof MultiPayloadAdaptrisMessage)
+		{
+			insert(data, getPayloadId(), (MultiPayloadAdaptrisMessage)m);
+		}
+		else
+		{
+			throw new InterlokException("Cannot insert payload into message type " + m.getClass().getName() + " as it does not support multiple payloads.");
+		}
+	}
+
+	/**
+	 * {@inheritDoc}.
+	 */
+	@Override
+	public void insert(InputStreamWithEncoding data, String id, MultiPayloadAdaptrisMessage m) throws InterlokException
+	{
+		if (id == null)
+		{
+			id = m.getCurrentPayloadId();
+		}
+		try
+		{
+			String encoding = defaultIfEmpty(getContentEncoding(), data.encoding);
+			if (isEmpty(encoding))
+			{
+				copyAndClose(data.inputStream, m.getOutputStream(id));
+			}
+			else
+			{
+				copyAndClose(data.inputStream, m.getWriter(id, encoding));
+				m.setContentEncoding(id, encoding);
+			}
+		}
+		catch (Exception e)
+		{
+			throw ExceptionHelper.wrapCoreException(e);
+		}
+	}
+}
---
+++ interlok-core/src/test/java/com/adaptris/core/common/MultiPayloadStreamOutputParameterTest.java
@@ -0,0 +1,114 @@
+package com.adaptris.core.common;
+
+import com.adaptris.core.AdaptrisMessage;
+import com.adaptris.core.DefaultMessageFactory;
+import com.adaptris.core.MultiPayloadAdaptrisMessage;
+import com.adaptris.core.MultiPayloadMessageFactory;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class MultiPayloadStreamOutputParameterTest
+{
+	private static final String ID = "cupcake";
+	private static final String PAYLOAD = "Cupcake ipsum dolor sit amet donut topping brownie wafer. Pie dessert tiramisu. Toffee candy canes fruitcake. Pastry cookie jelly-o tiramisu I love carrot cake lollipop cake halvah. Icing tart jelly ice cream. I love muffin chocolate cake sweet roll I love. Apple pie souffle I love I love pie cake. Carrot cake jelly beans cake.";
+	private static final String ENCODING = "UTF-8";
+
+	@Rule
+	public TestName testName = new TestName();
+
+	@Test
+	public void testInsert() throws Exception
+	{
+		MultiPayloadAdaptrisMessage message = (MultiPayloadAdaptrisMessage)new MultiPayloadMessageFactory().newMessage();
+		try (InputStream in = new ByteArrayInputStream(PAYLOAD.getBytes()))
+		{
+			InputStreamWithEncoding stream = new InputStreamWithEncoding(in, ENCODING);
+			MultiPayloadStreamOutputParameter parameter = new MultiPayloadStreamOutputParameter();
+			parameter.insert(stream, ID, message);
+		}
+		assertEquals(PAYLOAD, message.getContent(ID));
+	}
+
+	@Test
+	public void testInsertSetId() throws Exception
+	{
+		MultiPayloadAdaptrisMessage message = (MultiPayloadAdaptrisMessage)new MultiPayloadMessageFactory().newMessage();
+		try (InputStream in = new ByteArrayInputStream(PAYLOAD.getBytes()))
+		{
+			InputStreamWithEncoding stream = new InputStreamWithEncoding(in, ENCODING);
+			MultiPayloadStreamOutputParameter parameter = new MultiPayloadStreamOutputParameter();
+			parameter.setPayloadId(ID);
+			parameter.insert(stream, message);
+		}
+		assertEquals(PAYLOAD, message.getContent(ID));
+	}
+
+	@Test
+	public void testInsertUseDefaultId() throws Exception
+	{
+		MultiPayloadMessageFactory messageFactory = new MultiPayloadMessageFactory();
+		MultiPayloadAdaptrisMessage message = (MultiPayloadAdaptrisMessage)messageFactory.newMessage();
+		try (InputStream in = new ByteArrayInputStream(PAYLOAD.getBytes()))
+		{
+			InputStreamWithEncoding stream = new InputStreamWithEncoding(in, ENCODING);
+			MultiPayloadStreamOutputParameter parameter = new MultiPayloadStreamOutputParameter();
+			parameter.insert(stream, null, message);
+		}
+		assertEquals(PAYLOAD, message.getContent(messageFactory.getDefaultPayloadId()));
+	}
+
+	@Test
+	public void testInsertNoEncoding() throws Exception
+	{
+		MultiPayloadMessageFactory messageFactory = new MultiPayloadMessageFactory();
+		MultiPayloadAdaptrisMessage message = (MultiPayloadAdaptrisMessage)messageFactory.newMessage();
+		try (InputStream in = new ByteArrayInputStream(PAYLOAD.getBytes()))
+		{
+			InputStreamWithEncoding stream = new InputStreamWithEncoding(in, null);
+			MultiPayloadStreamOutputParameter parameter = new MultiPayloadStreamOutputParameter();
+			parameter.setContentEncoding(null);
+			parameter.insert(stream, ID, message);
+		}
+		assertEquals(PAYLOAD, message.getContent(ID));
+	}
+
+	@Test
+	public void testInsertNullStream() throws Exception
+	{
+		MultiPayloadMessageFactory messageFactory = new MultiPayloadMessageFactory();
+		MultiPayloadAdaptrisMessage message = (MultiPayloadAdaptrisMessage)messageFactory.newMessage();
+		try
+		{
+			InputStreamWithEncoding stream = new InputStreamWithEncoding(null, ENCODING);
+			MultiPayloadStreamOutputParameter parameter = new MultiPayloadStreamOutputParameter();
+			parameter.insert(stream, ID, message);
+		}
+		catch (Exception e)
+		{
+			// expected
+		}
+	}
+
+	@Test
+	public void testWrongMessageType()
+	{
+		try
+		{
+			MultiPayloadStreamOutputParameter parameter = new MultiPayloadStreamOutputParameter();
+			AdaptrisMessage message = DefaultMessageFactory.getDefaultInstance().newMessage();
+			parameter.insert(null, message);
+			fail();
+		}
+		catch (Exception e)
+		{
+			// expected
+		}
+	}
+}
