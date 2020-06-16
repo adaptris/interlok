f03a2f9c1c508fc39e4db084508164847b65a990
Aaron McGrath <aaron.mcgrath@adaptris.com>
2020-06-16 16:04:56 +0100

    INTERLOK-3277 Now set for removal 3.12

--- interlok-core/src/main/java/com/adaptris/core/RestartProduceExceptionHandler.java
+++ interlok-core/src/main/java/com/adaptris/core/RestartProduceExceptionHandler.java
@@ -27,7 +27,7 @@ import com.thoughtworks.xstream.annotations.XStreamAlias;
  * @deprecated since 3.10.2
  */
 @Deprecated
-@Removal(message = "If you need restarting capability use channel-restart-produce-exception-handler or wrap your producer into a standalone-producer and set restart services on failure.", version = "3.11.0")
+@Removal(message = "If you need restarting capability use channel-restart-produce-exception-handler or wrap your producer into a standalone-producer and set restart services on failure.", version = "3.12.0")
 @XStreamAlias("restart-produce-exception-handler")
 public class RestartProduceExceptionHandler extends ProduceExceptionHandlerImp {
 
