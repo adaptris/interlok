de9f6e58cb1d907aba43991ced2ee1f5ea252a43
Lewin Chan <lewin.chan@adaptris.com>
2020-06-18 13:34:45 +0100

    LGTM Ignore java/xxe since the default is "safe".

--- interlok-core/src/main/java/com/adaptris/security/keystore/XmlKeyInfoKeystoreProxy.java
+++ interlok-core/src/main/java/com/adaptris/security/keystore/XmlKeyInfoKeystoreProxy.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2015 Adaptris Ltd.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -84,8 +84,9 @@ class XmlKeyInfoKeystoreProxy extends SingleEntryKeystoreProxy {
   public void load() throws AdaptrisSecurityException, IOException {
 
     try (InputStream in = getKeystoreLocation().openInput()) {
+      // new RestrictedInstance does the right thing for XXE
       Document doc = DocumentBuilderFactoryBuilder.newRestrictedInstance().withNamespaceAware(true)
-          .newDocumentBuilder(DocumentBuilderFactory.newInstance()).parse(in);
+          .newDocumentBuilder(DocumentBuilderFactory.newInstance()).parse(in); // lgtm [java/xxe]
       loadCertificates(doc);
     }
     catch (SAXException | ParserConfigurationException | MarshalException e) {
