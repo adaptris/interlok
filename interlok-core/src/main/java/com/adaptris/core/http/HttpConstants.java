506095d25ce58980fb31676bc2df077d66c611f7
Lewin Chan <lewin.chan@adaptris.com>
2020-10-28 08:38:17 +0000

    Add some content-types as constants.

--- interlok-core/src/main/java/com/adaptris/core/http/HttpConstants.java
+++ interlok-core/src/main/java/com/adaptris/core/http/HttpConstants.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2015 Adaptris Ltd.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -249,7 +249,8 @@ public interface HttpConstants extends MimeConstants {
   /**
    * Fields Values.
    */
-  String WWW_FORM_URLENCODE = "application/x-www-form-urlencoded";
+  String WWW_FORM_URLENCODE = CONTENT_TYPE_WWW_FORM_URLENCODE;
+
   // The default server socket timeout, 6 secs
   /**
    * Default server socket timeout, 6secs
