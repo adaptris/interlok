e50de95a381bf019021ab37f780d93f02ca4f1dc
Aaron McGrath <aaron.mcgrath@adaptris.com>
2021-05-24 14:08:38 +0100

    INTERLOK-3326 Added default value annotation and javadoc.

--- interlok-core/src/main/java/com/adaptris/core/services/conditional/conditions/ConditionImpl.java
+++ interlok-core/src/main/java/com/adaptris/core/services/conditional/conditions/ConditionImpl.java
@@ -4,16 +4,22 @@ import org.apache.commons.lang3.BooleanUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.adaptris.annotation.InputFieldDefault;
 import com.adaptris.core.services.conditional.Condition;
 
 import lombok.Getter;
 import lombok.Setter;
 
 public abstract class ConditionImpl implements Condition {
+  
   protected transient Logger log = LoggerFactory.getLogger(this.getClass().getName());
 
   @Getter
   @Setter
+  @InputFieldDefault(value = "false")
+  /**
+   * Log the details of the conditions source and the result of the evaluation.
+   */
   private Boolean additionalLogging;
   
   protected void logCondition(String message, Object... args) {
