91bb2017d9ef4ce6be7d473c89a4e83d79df19b9
Lewin Chan <lewin.chan@adaptris.com>
2021-06-30 12:32:53 +0100

    INTERLOK-3818 Add InterlokLicense + InterlokAlias
    Added annotations to NullService as a "reference"

--- interlok-core/src/main/java/com/adaptris/core/NullService.java
+++ interlok-core/src/main/java/com/adaptris/core/NullService.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2015 Adaptris Ltd.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,16 +18,19 @@ package com.adaptris.core;
 
 import com.adaptris.annotation.AdapterComponent;
 import com.adaptris.annotation.ComponentProfile;
+import com.adaptris.annotation.InterlokAlias;
+import com.adaptris.annotation.InterlokLicence;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 
 /**
  * <p>
  * Null implementation of <code>Service</code>.
  * </p>
- * 
- * @config null-service
+ *
  */
 @XStreamAlias("null-service")
+@InterlokAlias("null-service")
+@InterlokLicence(InterlokLicence.Type.NONE)
 @AdapterComponent
 @ComponentProfile(summary = "A NO-OP service", tag = "service")
 public class NullService extends ServiceImp {
@@ -41,14 +44,17 @@ public class NullService extends ServiceImp {
     setUniqueId(uniqueId);
   }
 
+  @Override
   public void doService(AdaptrisMessage msg) throws ServiceException {
     // do nothing
   }
 
+  @Override
   protected void initService() throws CoreException {
     // do nothing
   }
 
+  @Override
   protected void closeService() {
     // do nothing
   }
