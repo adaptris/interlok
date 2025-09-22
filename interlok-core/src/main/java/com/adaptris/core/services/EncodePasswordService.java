2143eeadae6575051511264f58ea5c23e7727196
saurav-mukherjee-telus <saurav.mukherjee@telusagcg.com>
2025-09-22 11:41:53 +1000

    Remove comment line

--- interlok-core/src/main/java/com/adaptris/core/services/EncodePasswordService.java
+++ interlok-core/src/main/java/com/adaptris/core/services/EncodePasswordService.java
@@ -371,8 +371,7 @@ public class EncodePasswordService extends ServiceImp {
               } catch (PasswordException e) {
                   log.info("Password could not be decoded for line - {} ,exc - {}", line, e.getMessage());
               }
-
-              //
+              
               value = "'" + value + "'>";
           }
           line = line.replaceAll(passwordString, value);
