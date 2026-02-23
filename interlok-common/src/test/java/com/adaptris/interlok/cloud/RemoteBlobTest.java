1707b8bcd63d9e37375314b59cfb30826dc602dc
Josh Williamson <josh.williamson@telusagcg.com>
2026-02-23 15:05:30 +0000

    implemented errorSummary field in RemoteBlob

--- interlok-common/src/test/java/com/adaptris/interlok/cloud/RemoteBlobTest.java
+++ interlok-common/src/test/java/com/adaptris/interlok/cloud/RemoteBlobTest.java
@@ -1,6 +1,7 @@
 package com.adaptris.interlok.cloud;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 
@@ -26,4 +27,31 @@ public class RemoteBlobTest {
     assertEquals(10L, file.length());
   }
 
+  @Test
+  public void testErrorSummarySet() {
+    long now = System.currentTimeMillis();
+    RemoteBlob f = new RemoteBlob.Builder()
+        .setBucket("bucket")
+        .setName("name")
+        .setLastModified(now)
+        .setSize(10L)
+        .setErrorSummary("something went wrong")
+        .build();
+
+    assertEquals("something went wrong", f.getErrorSummary());
+  }
+
+  @Test
+  public void testErrorSummaryNull() {
+    long now = System.currentTimeMillis();
+    RemoteBlob f = new RemoteBlob.Builder()
+        .setBucket("bucket")
+        .setName("name")
+        .setLastModified(now)
+        .setSize(10L)
+        .setErrorSummary(null)
+        .build();
+
+    assertNull(f.getErrorSummary());
+  }
 }
