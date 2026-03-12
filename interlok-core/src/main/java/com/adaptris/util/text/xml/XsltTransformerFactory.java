c7ec2d4fa569fb839943974337af5945f41759ab
Josh Williamson <josh.williamson@telusagcg.com>
2026-03-12 13:39:08 +0000

    NWK-2895 Saxon Extensions (#2058)
    * XsltTransformerFactory now supports Saxon extensions for a class implementing the SaxonExtensionRegistrar interface
    * dependency vulnerability remediation
    * CVE-2026-27727 remediation

--- interlok-core/src/main/java/com/adaptris/util/text/xml/XsltTransformerFactory.java
+++ interlok-core/src/main/java/com/adaptris/util/text/xml/XsltTransformerFactory.java
@@ -16,6 +16,8 @@
 
 package com.adaptris.util.text.xml;
 
+import java.util.Optional;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.Transformer;
@@ -23,6 +25,8 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
 import org.xml.sax.EntityResolver;
 import org.xml.sax.InputSource;
@@ -31,25 +35,53 @@ import com.adaptris.annotation.AdvancedConfig;
 import com.adaptris.annotation.DisplayOrder;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 
+import lombok.Getter;
+import lombok.Setter;
+import net.sf.saxon.Configuration;
+import net.sf.saxon.TransformerFactoryImpl;
+
 /**
+ * An {@link XmlTransformerFactory} implementation that creates XSLT {@link Transformer} instances.
+ *
  * <p>
- * The XsltTransformerFactory is responsible for creating the {@link Transformer}.
+ * By default, the JDK's built-in {@link TransformerFactory} is used. You can override this by
+ * setting {@code transformerFactoryImpl} to the fully-qualified class name of an alternative
+ * {@link TransformerFactory} implementation — for example, Saxon's
+ * {@code net.sf.saxon.TransformerFactoryImpl} for XSLT 2.0/3.0 support.
  * </p>
+ *
  * <p>
- * The {@link Transformer} is used to actually perform a document transformation.
+ * When a Saxon {@link TransformerFactory} is detected, optional Saxon extension functions can be
+ * registered via a {@link SaxonExtensionRegistrar} implementation. Specify the fully-qualified
+ * class name of your {@link SaxonExtensionRegistrar} via {@code extensionRegistrarImplementation}.
+ * If not set, no extensions are registered.
+ * </p>
+ *
+ * <p>
+ * This factory also overrides the URL-based transformer creation to parse the XSL stylesheet
+ * directly from its URL as a DOM {@link Document}, preserving file location context so that
+ * relative {@code xsl:import} and {@code xsl:include} paths resolve correctly.
  * </p>
  *
  * @config xslt-transformer-factory
- *
  * @author amcgrath
  */
 
 @XStreamAlias("xslt-transformer-factory")
-@DisplayOrder(order = { "transformerFactoryImpl", "failOnRecoverableError" })
+@DisplayOrder(order = { "transformerFactoryImpl", "extensionRegistrarImplementation", "failOnRecoverableError" })
 public class XsltTransformerFactory extends XmlTransformerFactoryImpl {
 
+  private transient final Logger log = LoggerFactory.getLogger(this.getClass());
+
+  @Getter
+  @Setter
   @AdvancedConfig
   private String transformerFactoryImpl;
+
+  @Getter
+  @Setter
+  @AdvancedConfig
+  private String extensionRegistrarImplementation;
 
   public XsltTransformerFactory() {
     super();
@@ -75,31 +107,56 @@ public class XsltTransformerFactory extends XmlTransformerFactoryImpl {
     return configure(newInstance()).newTransformer(new DOMSource(xmlDoc, url));
   }
 
-    /**
-   * @return the transformerFactoryImpl
-   */
-  public String getTransformerFactoryImpl() {
-    return transformerFactoryImpl;
-  }
-
-  /**
-   * Specify the transformer factory that will be used.
-   * <p>
-   * If you have both saxon and xalan (for instance) available on the classpath; and you want to explicitly use the xalan implementation
-   * then you could put {@code org.apache.xalan.processor.TransformerFactoryImpl} here to force it to use Xalan or
-   * {@code net.sf.saxon.TransformerFactoryImpl} to force it to use Saxon.
-   * <p>
-   *
-   * @param s
-   *          he transformerFactoryImpl to set, if not specified the JVM default is used {@link TransformerFactory#newInstance()}.
-   */
-  public void setTransformerFactoryImpl(String s) {
-    transformerFactoryImpl = s;
-  }
-
+  @Override
   protected TransformerFactory newInstance() {
-    return StringUtils.isEmpty(getTransformerFactoryImpl()) ? TransformerFactory.newInstance()
+    TransformerFactory tf = StringUtils.isEmpty(getTransformerFactoryImpl())
+        ? TransformerFactory.newInstance()
         : TransformerFactory.newInstance(getTransformerFactoryImpl(), null);
+
+    getSaxonConfiguration(tf).ifPresent(config -> {
+      log.debug("Registering Saxon extensions with config: {}", config);
+      getExtensionRegistrar().register(config);
+    });
+
+    return tf;
   }
 
+  protected Optional<Configuration> getSaxonConfiguration(TransformerFactory tf) {
+    log.debug("Getting SaxonConfiguration if applicable");
+
+    if (tf == null) {
+      return Optional.empty();
+    }
+
+    // Saxon-HE and Saxon-EE factories both derive from TransformerFactoryImpl.
+    if (tf instanceof TransformerFactoryImpl) {
+      log.debug("Retrieving Saxon Configuration from {}", tf.getClass().getName());
+      return Optional.of(((TransformerFactoryImpl) tf).getConfiguration());
+    }
+    return Optional.empty();
+  }
+
+  public SaxonExtensionRegistrar getExtensionRegistrar() {
+    return StringUtils.isNotBlank(getExtensionRegistrarImplementation())
+        ? loadExtensionRegistrar(getExtensionRegistrarImplementation())
+        : new NoExtensions();
+  }
+
+  private SaxonExtensionRegistrar loadExtensionRegistrar(String className) {
+    try {
+      Class<?> clazz = Class.forName(className);
+      if (SaxonExtensionRegistrar.class.isAssignableFrom(clazz)) {
+        log.debug("Found SaxonExtensionRegistrar implementation class: {}", className);
+        return (SaxonExtensionRegistrar) clazz.getDeclaredConstructor().newInstance();
+      } else {
+        log.warn("Class {} does not implement SaxonExtensionRegistrar", className);
+      }
+    } catch (Throwable t) {
+      if (t instanceof  InterruptedException) {
+        Thread.currentThread().interrupt();
+      }
+      log.warn("Failed to load SaxonExtensionRegistrar implementation: {}", className, t);
+    }
+    return new NoExtensions();
+  }
 }
