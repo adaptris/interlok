f19d7cc1fc1f511552150e15b3dacea870a9909b
saurav-mukherjee-telus <saurav.mukherjee@telusagcg.com>
2026-03-16 16:37:18 +1100

    Updated Unit tests

--- interlok-core/src/main/java/com/adaptris/core/jms/DefinedJmsProducer.java
+++ interlok-core/src/main/java/com/adaptris/core/jms/DefinedJmsProducer.java
@@ -147,7 +147,6 @@ public abstract class DefinedJmsProducer extends JmsProducerImpl {
 
   protected abstract Destination createTemporaryDestination() throws JMSException;
 
-  private Message sendMessage(AdaptrisMessage msg, Destination destination, Destination replyTo) throws JMSException {
     Message jmsMsg = translate(msg, replyTo);
     if (!perMessageProperties()) {
       producerSession().getProducer().send(destination, jmsMsg);
